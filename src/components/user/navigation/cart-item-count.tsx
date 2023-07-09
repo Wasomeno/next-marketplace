bfa8f44aa9c6d77172a635b16eb02887231ec3d1
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-09 20:05:56 +0700

    refactor: add enabled condition on cartItemsAmount query

--- src/components/user/navigation/cart-item-count.tsx
+++ src/components/user/navigation/cart-item-count.tsx
@@ -5,16 +5,19 @@ import { useQuery } from "@tanstack/react-query";
 export function CartItemCount({ user }: { user: string }) {
   const cartItemsAmount = useQuery(
     ["cartItemCount", user],
-    async () => await getCartItemsCount()
+    async () => await getCartItemsCount(),
+    {
+      enabled: user !== undefined,
+    }
   );
 
   async function getCartItemsCount() {
-    return await fetch(`http://localhost:3000//api/users/${user}/cart/count`, {
+    return await fetch(`/api/users/${user}/cart/count`, {
       method: "GET",
     }).then((count) => count.json());
   }
 
-  if (!cartItemsAmount.isLoading) {
+  if (!cartItemsAmount.isLoading || !cartItemsAmount.data) {
     return (
       <div className="flex-items-center absolute -right-2.5 -top-2 flex h-5 w-5 justify-center rounded-full  border-2 border-white bg-red-600 text-xs font-semibold text-white lg:h-6 lg:w-6 lg:text-sm">
         {cartItemsAmount.data}
