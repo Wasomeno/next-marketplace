20e2c77f73fe6dfd944da3c8a4779c269eb5224b
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-09 16:31:32 +0700

    add: footer section for user pages

---
+++ src/components/user/footer.tsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer className="hidden border-t p-6 lg:block">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <span className="text-base font-medium tracking-wide lg:text-lg">
+            Next Marketplace
+          </span>
+          <p className="text-xs tracking-wider lg:text-sm">
+            easy shopping with the best prices
+          </p>
+        </div>
+        <div className="text-xs tracking-wider lg:text-sm">
+          A tokopedia clone made by me
+        </div>
+      </div>
+    </footer>
+  );
+};
