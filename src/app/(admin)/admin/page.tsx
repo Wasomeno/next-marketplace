99d934253fe52b57ec41e5a27be5efe4f34cbfc9
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-09 15:45:27 +0700

    add: dashboard page for admin

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div className="flex w-full flex-1 flex-col p-5">
+      <div className="mb-4 flex items-center">
+        <h1 className="text-base font-medium tracking-wider lg:text-2xl">
+          Dashboard
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
