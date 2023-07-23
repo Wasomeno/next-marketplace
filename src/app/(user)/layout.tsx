e8767107cbf2f154cec2474cca60ba9d330cbc00
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-23 20:21:38 +0700

    refactor: remove min-h-screen

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -8,7 +8,7 @@ interface UserLayoutProps {
 
 export default async function UserLayout({ children }: UserLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-1 flex-col gap-4 bg-white lg:gap-6">
+    <div className="flex flex-1 flex-col gap-4 bg-white lg:gap-6">
       <NavigationMain />
       {children}
       <Footer />
