fba3cc52c07e98a54eb75ca7b271fceca91a65e9
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-23 20:21:22 +0700

    refactor: add conditions when user dont have a cart, user already have a cart & selected item already on the cart, and when the user already have a cart but the selected item does not exists

--- src/app/api/users/[user]/cart/route.ts
+++ src/app/api/users/[user]/cart/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
+import { CartItem } from "@prisma/client";
 
 export async function GET(request: Request, context: { params: any }) {
   const { user } = context.params;
@@ -15,18 +16,63 @@ export async function GET(request: Request, context: { params: any }) {
 export async function POST(request: Request, context: { params: any }) {
   const { user } = context.params;
   const body = await request.json();
+  const cart = await prisma.cart.findUnique({
+    where: { user_email: user },
+    include: { items: true },
+  });
+
+  const itemExists = cart?.items.some(
+    (item) => item.product_id === body.product.id
+  );
+
+  if (!cart) {
+    try {
+      await prisma.cart.create({
+        data: {
+          items: {
+            create: {
+              product: { connect: { id: parseInt(body.product.id) } },
+              amount: parseInt(body.product.amount),
+            },
+          },
+          user: { connect: { email: user } },
+        },
+      });
+      return NextResponse.json({ message: "success" });
+    } catch (error) {
+      return NextResponse.error();
+    }
+  }
+
+  if (cart && itemExists) {
+    try {
+      const cartItem = cart.items.find(
+        (item: CartItem) => item.product_id === body.product.id
+      );
+      await prisma.cart.update({
+        where: { user_email: user },
+        data: {
+          items: {
+            update: {
+              where: {
+                id: cartItem?.id,
+                product_id: parseInt(body.product.id),
+              },
+              data: { amount: (cartItem?.amount as number) + 1 },
+            },
+          },
+        },
+      });
+      return NextResponse.json({ message: "success" });
+    } catch (error) {
+      return NextResponse.error();
+    }
+  }
+
   try {
-    await prisma.cart.upsert({
-      create: {
-        items: {
-          create: {
-            product: { connect: { id: parseInt(body.product.id) } },
-            amount: parseInt(body.product.amount),
-          },
-        },
-        user: { connect: { email: user } },
-      },
-      update: {
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
         items: {
           create: {
             product: { connect: { id: parseInt(body.product.id) } },
@@ -34,7 +80,6 @@ export async function POST(request: Request, context: { params: any }) {
           },
         },
       },
-      where: { user_email: user },
     });
     return NextResponse.json({ message: "success" });
   } catch (error) {
