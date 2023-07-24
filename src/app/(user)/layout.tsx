c08a2f9026af6a099e0a1718a066dd89498e4c77
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-24 19:20:45 +0700

    refactor: remove NavigationMain, NavigationMobile & Footer components

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,3 @@
-import { Footer } from "@/components/user/footer";
-import { NavigationMain } from "@/components/user/navigation/navigation-main";
-import { NavigationMobile } from "@/components/user/navigation/navigation-mobile";
-
 interface UserLayoutProps {
   children: React.ReactNode;
 }
@@ -9,10 +5,7 @@ interface UserLayoutProps {
 export default async function UserLayout({ children }: UserLayoutProps) {
   return (
     <div className="flex min-h-screen flex-1 flex-col gap-4 bg-white lg:gap-6">
-      <NavigationMain />
       {children}
-      <Footer />
-      <NavigationMobile />
     </div>
   );
 }
