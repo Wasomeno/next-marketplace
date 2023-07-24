79017acd7381f4ef5718121cc54c4d56b6458481
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-24 19:21:24 +0700

    refactor: remove sign up link

--- src/components/user/navigation/user-menu/index.tsx
+++ src/components/user/navigation/user-menu/index.tsx
@@ -11,18 +11,12 @@ export const UserMenu = ({ session }: { session: Session | null }) => {
       <UserMenuMobile session={session} />
     </>
   ) : (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center justify-center gap-2">
       <Link
         href="/login"
-        className="rounded-md bg-neutral-200 px-3 py-2 font-sans text-sm"
+        className="rounded-md bg-blue-400 px-3 py-1.5 font-sans text-xs font-medium tracking-tight text-white lg:text-sm"
       >
         Sign In
-      </Link>
-      <Link
-        href="/register"
-        className="rounded-md border border-neutral-200 px-3 py-2 font-sans text-sm"
-      >
-        Sign Up
       </Link>
     </div>
   );
