17ccb8e20379b1d82e6a8e384bdb38cb622bf25c
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-24 19:22:18 +0700

    refactor: add separator component

--- src/components/user/navigation/navigation-main.tsx
+++ src/components/user/navigation/navigation-main.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { IoCartOutline } from "react-icons/io5";
 
+import { Separator } from "@/components/ui/separator";
+
 import { authOptions } from "../../../../pages/api/auth/[...nextauth]";
 import { CartItemCount } from "./cart-item-count";
 import { SearchProduct } from "./search-product";
@@ -11,7 +13,7 @@ import { UserMenu } from "./user-menu";
 export async function NavigationMain() {
   const session = await getServerSession(authOptions);
   return (
-    <div className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-b-gray-200 bg-white px-4 shadow-sm">
+    <div className="sticky top-0 z-20 flex h-16 w-full items-center justify-between border-b border-b-gray-200 bg-white px-4 shadow-sm">
       <div className="flex w-full items-center lg:w-3/6">
         <Link
           href="/"
@@ -27,14 +29,16 @@ export async function NavigationMain() {
         </Link>
         <SearchProduct />
       </div>
-      <div className="flex w-2/6 items-center justify-evenly lg:w-1/6">
+      <div className="flex w-3/6 items-center justify-evenly lg:w-44">
         <Link href="/cart" className="relative font-sans">
           <IoCartOutline className="h-6 w-6 fill-slate-500 lg:h-7 lg:w-7" />
           <CartItemCount user={session?.user?.email as string} />
         </Link>
-        <div className="flex justify-center">
-          <UserMenu session={session} />
-        </div>
+        <Separator
+          className="h-7 w-px rounded-full bg-slate-200"
+          orientation="vertical"
+        />
+        <UserMenu session={session} />
       </div>
     </div>
   );
