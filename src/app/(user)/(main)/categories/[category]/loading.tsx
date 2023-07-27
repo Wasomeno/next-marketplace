bc438b03ec2449b459ec932b49974b82e00abb2c
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-27 19:27:32 +0700

    refactor: change the fallback loading page to match the real page

--- src/app/(user)/(main)/categories/[category]/loading.tsx
+++ src/app/(user)/(main)/categories/[category]/loading.tsx
@@ -1,22 +1,30 @@
+import { Button } from "@/components/ui/button";
 import ProductCard from "@/components/user/product-card";
 
-import { ProductSorterSkeleton } from "../../[product]/components/product-sorter";
-import { CategoryProductsFilterSkeleton } from "./components/product-filter";
+import { ProductSorterSkeleton } from "../../../../../components/product-sorter";
 
 export default function CategoryLoading() {
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-6">
-      <div className="flex h-36 w-full items-center justify-between border-y bg-slate-50 px-16 lg:h-72 ">
+      <div className="flex h-36 w-full items-center justify-between border-y bg-slate-50 px-10 lg:h-72 lg:px-16 ">
         <div className="h-14 w-72 animate-pulse rounded-md bg-slate-200" />
       </div>
-      <div className="relative flex w-full justify-center gap-10 px-4 lg:w-11/12">
-        <CategoryProductsFilterSkeleton />
-        <div className="w-full lg:w-9/12">
+      <div className="relative flex w-full justify-center gap-10 px-4 lg:px-10">
+        <div className="w-full">
           <div className="mb-4 flex items-center justify-between">
-            <h6 className="mb-4 text-sm font-medium lg:text-lg">Products</h6>
-            <ProductSorterSkeleton />
+            <h6 className="text-sm font-medium lg:text-lg">Products</h6>
+            <div className="flex items-center gap-2">
+              <ProductSorterSkeleton />
+              <Button
+                disabled
+                variant="defaultOutline"
+                className="border-slate-200"
+              >
+                Filter
+              </Button>
+            </div>
           </div>
-          <div className="grid grid-cols-8 gap-4">
+          <div className="grid grid-cols-10 gap-4">
             {["dummy", "dummy", "dummy", "dummy", "dummy"].map(
               (dummy, index) => (
                 <ProductCard.Skeleton key={index} />
