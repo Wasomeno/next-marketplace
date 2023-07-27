c3ddcfebeb9f258e6e8159126538983ef72c9e79
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-27 16:17:44 +0700

    refactor: add product price filters & sorting when query the products

--- src/app/(user)/(main)/search/page.tsx
+++ src/app/(user)/(main)/search/page.tsx
@@ -16,13 +16,23 @@ export async function generateMetadata({
   };
 }
 
-export default async function SearchPage({
-  searchParams,
-}: {
-  searchParams: { q: string };
-}) {
+export default async function SearchPage({ searchParams }: Props) {
+  const priceMin = searchParams.pmin;
+  const priceMax = searchParams.pmax;
+  const sort = searchParams.sort
+    ? [searchParams.sort?.split(".") as string]
+    : [];
+
   const products = await prisma.product.findMany({
-    where: { name: { contains: searchParams.q } },
+    where: {
+      name: { contains: searchParams.q as string },
+      price: {
+        lte: priceMax ? parseInt(priceMax as string) : 5000000,
+        gte: priceMin ? parseInt(priceMin as string) : 100,
+      },
+    },
+    orderBy: !sort.length ? { price: "asc" } : Object.fromEntries(sort as []),
+
     include: { category: true, images: { select: { image_url: true } } },
   });
   return <SearchPageContainer products={products} />;
