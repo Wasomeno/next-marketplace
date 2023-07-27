81ce4d99e4875f35409ddb64fd6c03fb0782d1ea
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-28 01:27:04 +0700

    feat: add ref for search input so it can be used on onSubmit function, add blur when form onSubmit

--- src/components/user/navigation/search-product.tsx
+++ src/components/user/navigation/search-product.tsx
@@ -1,22 +1,27 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { Input } from "@/components/ui/input";
 
 export function SearchProduct() {
   const [searchParams, setSearchParams] = useState("");
   const router = useRouter();
+
+  const searchInputRef = useRef<HTMLInputElement>();
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         router.push("/search?q=" + searchParams);
+        searchInputRef.current?.blur();
       }}
       className="w-5/6 lg:w-5/6"
     >
       <Input
+        ref={searchInputRef as any}
         placeholder="Search..."
         className="h-8 w-full rounded-md border bg-slate-50 p-2 font-sans text-xs lg:h-10 lg:text-sm"
         onChange={(event) => setSearchParams(event.target.value)}
