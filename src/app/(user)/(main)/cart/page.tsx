7a4e4108f66539edb61e519d89c2b231083c5239
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-27 19:25:17 +0700

    refactor: replace getServerSession with useSession inside CartItems & CartCheckOutDialog , remove if condition when there's no session because it's already been handled by middleware, & merge types into a single type CartItem

--- src/app/(user)/(main)/cart/page.tsx
+++ src/app/(user)/(main)/cart/page.tsx
@@ -1,22 +1,13 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
+import { CartItemsSection } from "@/components/user/cart/cart-items-section";
 import { Cart, Prisma } from "@prisma/client";
 
-import { authOptions } from "../../../api/auth/[...nextauth]/route";
-import { CartItemsSection } from "./components/cart-items-section";
-
-export type CartItemWithProductPrice = Prisma.CartItemGetPayload<{
+export type CartItem = Prisma.CartItemGetPayload<{
   select: {
     amount: true;
     id: true;
-    product: { select: { price: true } };
+    product: { include: { price: true; images: true } };
     product_id: true;
   };
-}>;
-
-export type CartItemWithProductImage = Prisma.CartItemGetPayload<{
-  include: { product: { include: { images: true } } };
 }>;
 
 export const metadata = {
@@ -24,15 +15,10 @@ export const metadata = {
 };
 
 export default async function Cart() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-
-  if (!userEmail) redirect("/login");
-
   return (
     <div className="flex flex-1 flex-col">
       <h1 className="mb-4 px-5 text-base lg:px-8 lg:text-xl">Shopping Cart</h1>
-      <CartItemsSection user={session.user?.email as string} />
+      <CartItemsSection />
     </div>
   );
 }
