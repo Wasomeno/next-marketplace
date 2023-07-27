53a47353ea2bc6b4924c9b9adedbf67c2b2897bd
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-27 16:12:21 +0700

    feat: add middleware for checking if users or admins is in a session

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+    signOut: "/",
+  },
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname.startsWith("/admin")) {
+        return token?.role === "admin";
+      } else {
+        return token !== null;
+      }
+    },
+  },
+});
+
+export const config = { matcher: ["/cart", "/orders", "/admin"] };
