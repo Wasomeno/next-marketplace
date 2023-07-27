6d9eafb6fdbc062b4ca386eb6eb804ce96ac0e78
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-27 16:13:34 +0700

    refactor: remove unused import

--- src/components/user/product-card.tsx
+++ src/components/user/product-card.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaStar } from "react-icons/fa";
-import { utapi } from "uploadthing/server";
 
 const ProductCard = ({
   image,
