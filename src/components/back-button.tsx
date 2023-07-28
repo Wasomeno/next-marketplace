ae9496566155df06e4ba5743c15198c69b77f04f
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-28 17:29:00 +0700

    feat: add back button component

---
+++ src/components/back-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { BsArrowLeft } from "react-icons/bs";
+
+import { Button } from "./ui/button";
+
+export const BackButton = ({
+  size,
+  text,
+}: {
+  size?: string | number;
+  text?: string;
+}) => {
+  const { back } = useRouter();
+  return (
+    <Button onClick={back} className="flex gap-2 bg-white px-3 lg:hidden">
+      <BsArrowLeft size={size ?? 16} />
+      {text && <span className="text-sm font-medium">{text}</span>}
+    </Button>
+  );
+};
