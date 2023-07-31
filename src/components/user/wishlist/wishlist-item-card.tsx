b7437b735ddee7d9382c4b98880f4758e052a118
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-31 21:39:40 +0700

    feat: add wishlist item card & wishlist items section for wishlist

---
+++ src/components/user/wishlist/wishlist-item-card.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import Image from "next/image";
+import { Dispatch, SetStateAction, useTransition } from "react";
+import { BiTrash } from "react-icons/bi";
+import { BsCheck } from "react-icons/bs";
+import { toast } from "react-toastify";
+
+import { removeProductFromWishlist } from "@/app/actions/wishlist";
+import { Button } from "@/components/ui/button";
+import { Prisma } from "@prisma/client";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+export const WishListItemCard = ({
+  setSelectedItems,
+  item,
+}: {
+  setSelectedItems: Dispatch<SetStateAction<number[]>>;
+  item: Prisma.WishlistItemGetPayload<{
+    include: { product: { include: { images: true } } };
+  }>;
+}) => {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <div className="flex items-center gap-4 border-t p-4">
+      <Checkbox.Root
+        onClick={() =>
+          setSelectedItems((selectedItems) => {
+            if (selectedItems.includes(item.product_id)) {
+              return selectedItems.filter(
+                (selectedItem) => selectedItem !== item.product_id
+              );
+            } else {
+              return [...selectedItems, item.product_id];
+            }
+          })
+        }
+        className="flex h-[15px] w-[15px] items-center justify-center rounded-sm border border-slate-400 bg-slate-50 lg:h-5 lg:w-5"
+      >
+        <Checkbox.Indicator color="black">
+          <BsCheck size="18" />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <div className="flex w-full items-end justify-between">
+        <div className="flex w-full gap-4">
+          <div className="w-[60px] lg:w-[120px]">
+            <div className="relative h-16 w-full rounded-md bg-slate-200 lg:h-32">
+              <Image
+                src={item.product.images[0].image_url}
+                alt="product-image"
+                fill
+              />
+            </div>
+          </div>
+          <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
+            <span className="text-sm tracking-wide lg:text-base">
+              Rp {item.product.price}
+            </span>
+            <span className="text-xs tracking-wide text-slate-500 lg:text-base">
+              {item.product.name}
+            </span>
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="danger"
+            size="sm"
+            className="h-7 w-7 p-2 text-white lg:h-8 lg:w-8"
+            onClick={() =>
+              startTransition(async () => {
+                await removeProductFromWishlist(item.product.id, "/wishlist");
+                toast.error(`Removed ${item.product.name} from wishlist`);
+              })
+            }
+          >
+            <BiTrash size="14" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const WishListItemCardSkeleton = () => {
+  return (
+    <div className="flex items-center gap-4 border-t p-4">
+      <Checkbox.Root
+        disabled
+        className="flex h-[15px] w-[15px] items-center justify-center rounded-sm border border-slate-400 bg-slate-50 lg:h-5 lg:w-5"
+      >
+        <Checkbox.Indicator color="black">
+          <BsCheck size="18" />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <div className="flex w-full items-end justify-between">
+        <div className="flex w-full gap-4">
+          <div className="w-[60px] lg:w-[120px]">
+            <div className="relative h-16 w-full animate-pulse rounded-md bg-slate-300 lg:h-32" />
+          </div>
+          <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
+            <div className="h-[0.75rem] w-32 animate-pulse rounded-md bg-slate-300 lg:h-[1rem]" />
+            <div className="h-[0.75rem] w-40 animate-pulse rounded-md bg-slate-300 lg:h-[1rem]" />
+          </div>
+        </div>
+        <div>
+          <Button
+            disabled
+            variant="danger"
+            size="sm"
+            className="h-7 w-7 p-2 text-white lg:h-8 lg:w-8"
+          >
+            <BiTrash size="14" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/user/wishlist/wishlist-items-section.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState, useTransition } from "react";
+import { toast } from "react-toastify";
+
+import { addAllToCart } from "@/app/actions/wishlist";
+import { ProductSorter } from "@/components/product-sorter";
+import { Button } from "@/components/ui/button";
+import { Prisma } from "@prisma/client";
+
+import { WishListItemCard } from "./wishlist-item-card";
+
+export const WishlistItemsSection = ({
+  items,
+}: {
+  items:
+    | Prisma.WishlistItemGetPayload<{
+        include: { product: { include: { images: true } } };
+      }>[]
+    | undefined;
+}) => {
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [isPending, startTransition] = useTransition();
+  return (
+    <div className="flex flex-1 flex-col justify-between lg:flex-row">
+      <div className="w-full lg:w-7/12">
+        <div className="flex items-center justify-between px-4 lg:px-0">
+          <span className="font-sans text-xs font-medium text-slate-400 lg:text-sm">
+            {items?.length ?? 0} items
+          </span>
+          <ProductSorter />
+        </div>
+        <div className="mt-2">
+          {items?.length ? (
+            items?.map((item) => (
+              <WishListItemCard
+                key={item.id}
+                item={item}
+                setSelectedItems={setSelectedItems}
+              />
+            ))
+          ) : (
+            <div className="flex h-96 flex-col items-center justify-center">
+              <span className="text-sm font-medium text-slate-400 lg:text-base">
+                No items
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="sticky bottom-0 w-full lg:w-2/6">
+        <div className="flex flex-col justify-between border-t border-slate-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] lg:h-72 lg:rounded-lg lg:border lg:border-slate-300 lg:p-4 lg:shadow-none">
+          <div className="">
+            <span className="hidden text-xs font-medium lg:inline lg:text-base">
+              Selected Items Summary
+            </span>
+            <div className="my-2 flex justify-between">
+              <span className="text-xs text-slate-500 lg:text-base">
+                Subtotal
+              </span>
+              <span className="text-sm lg:text-lg">Rp 0</span>
+            </div>
+          </div>
+          <Button
+            disabled={!selectedItems.length}
+            variant="default"
+            className="my-1 w-full rounded-lg border bg-blue-400  py-3 text-xs font-medium text-slate-50 lg:text-sm"
+            onClick={() =>
+              startTransition(async () => {
+                await addAllToCart(selectedItems);
+                toast.success("Added all selected items to cart");
+              })
+            }
+          >
+            Add all to Cart
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
