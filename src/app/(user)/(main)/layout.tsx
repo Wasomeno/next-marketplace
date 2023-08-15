baeb024dbc154dcbece23ba3f9fc20538564399d
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-15 23:33:10 +0700

    refactor: change the import for navigations to the updated one

--- src/app/(user)/(main)/layout.tsx
+++ src/app/(user)/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer } from "@/components/user/footer";
-import { NavigationMain } from "@/components/user/navigation/navigation-main";
-import { NavigationMobile } from "@/components/user/navigation/navigation-mobile";
+import { UserMainNavigation } from "@/components/user/navigation/user-main-navigation";
+import { UserMobileNavigation } from "@/components/user/navigation/user-mobile-navigation";
 
 interface UserLayoutProps {
   children: React.ReactNode;
@@ -8,11 +8,11 @@ interface UserLayoutProps {
 
 export default async function UserMainLayout({ children }: UserLayoutProps) {
   return (
-    <div className="flex flex-1 flex-col gap-2 bg-white lg:gap-6">
-      <NavigationMain />
+    <div className="flex flex-1 flex-col gap-2 bg-white dark:bg-slate-950 lg:gap-6">
+      <UserMainNavigation />
       {children}
       <Footer />
-      <NavigationMobile />
+      <UserMobileNavigation />
     </div>
   );
 }
