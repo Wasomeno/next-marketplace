58fc5a9436c6be1b2ca00fe52017698e04cc4f88
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-15 23:36:32 +0700

    feat: add config directory & paths config for paths in user mobile navigation

---
+++ src/config/paths.ts
@@ -0,0 +1,27 @@
+import { IconType } from "react-icons";
+import { BiCategory } from "react-icons/bi";
+import { BsCardList, BsHouseDoor } from "react-icons/bs";
+
+type UserMobilePath = {
+  title: string;
+  href: string;
+  Icon: IconType;
+};
+
+export const userMobilePaths: UserMobilePath[] = [
+  {
+    title: "Home",
+    href: "/",
+    Icon: BsHouseDoor,
+  },
+  {
+    title: "Categories",
+    href: "/categories",
+    Icon: BiCategory,
+  },
+  {
+    title: "Orders",
+    href: "/orders",
+    Icon: BsCardList,
+  },
+];
