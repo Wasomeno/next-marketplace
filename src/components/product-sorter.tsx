b7ecab4180ced50ae8f4164430356119524c79c1
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 19:04:46 +0700

    refactor(tailwind): change border color for dropdown trigger and contet to gray-800

--- src/components/product-sorter.tsx
+++ src/components/product-sorter.tsx
@@ -1,22 +1,22 @@
-"use client";
+"use client"
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { BiChevronRight } from "react-icons/bi";
-import { HiArrowsUpDown } from "react-icons/hi2";
+import { useEffect, useState } from "react"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { BiChevronRight } from "react-icons/bi"
+import { HiArrowsUpDown } from "react-icons/hi2"
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 import {
   Dropdown,
   DropdownContent,
   DropdownItem,
   DropdownTrigger,
-} from "@/components/ui/dropdown";
+} from "@/components/ui/dropdown"
 
 export interface ProductSort {
-  id: number;
-  label: string;
-  value: string;
+  id: number
+  label: string
+  value: string
 }
 
 const sortOptions: ProductSort[] = [
@@ -40,41 +40,41 @@ const sortOptions: ProductSort[] = [
     label: "Name from Z to A",
     value: "name.desc",
   },
-];
+]
 
 export function ProductSorter() {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams()
 
   const getActiveSort = () => {
-    const sortParams = searchParams.get("sort");
-    const activeSort = sortOptions.find((sort) => sort.value === sortParams);
-    return activeSort;
-  };
+    const sortParams = searchParams.get("sort")
+    const activeSort = sortOptions.find((sort) => sort.value === sortParams)
+    return activeSort
+  }
 
   const [selectedSort, setSelectedSort] = useState<ProductSort | undefined>(
     getActiveSort()
-  );
+  )
 
-  const router = useRouter();
-  const path = usePathname();
+  const router = useRouter()
+  const path = usePathname()
 
   const createSearchParams = () => {
-    const newParams = new URLSearchParams(searchParams.toString());
-    newParams.set("sort", selectedSort?.value as string);
-    return newParams.toString();
-  };
+    const newParams = new URLSearchParams(searchParams.toString())
+    newParams.set("sort", selectedSort?.value as string)
+    return newParams.toString()
+  }
 
   useEffect(() => {
     if (selectedSort) {
-      const url = `${path}?${createSearchParams()}`;
-      router.push(url);
+      const url = `${path}?${createSearchParams()}`
+      router.push(url)
     }
-  }, [selectedSort]);
+  }, [selectedSort])
 
   return (
     <Dropdown>
       <DropdownTrigger asChild>
-        <button className="flex h-8 w-8 items-center justify-center rounded-md border bg-white dark:bg-slate-950 dark:border-gray-700 px-3 text-sm outline-0 lg:h-10 lg:w-52 lg:justify-between">
+        <button className="flex h-8 w-8 items-center justify-center rounded-md border bg-white px-3 text-sm outline-0 dark:border-gray-800 dark:bg-slate-950 lg:h-10 lg:w-52 lg:justify-between">
           <span className="hidden font-medium lg:block">
             {selectedSort ? selectedSort.label : "Select Sort"}
           </span>
@@ -92,7 +92,7 @@ export function ProductSorter() {
       </DropdownTrigger>
       <DropdownContent
         align="end"
-        className="flex w-52 flex-col overflow-hidden rounded-md border bg-white dark:border-slate-700 dark:bg-slate-950 text-sm shadow-sm lg:rounded-b-md lg:rounded-t-none lg:border-t-0"
+        className="flex w-52 flex-col overflow-hidden rounded-md border bg-white text-sm shadow-sm dark:border-gray-800 dark:bg-slate-950 lg:rounded-b-md lg:rounded-t-none lg:border-t-0"
       >
         {sortOptions.map((sort) => (
           <DropdownItem key={sort.id} asChild>
@@ -106,23 +106,23 @@ export function ProductSorter() {
         ))}
       </DropdownContent>
     </Dropdown>
-  );
+  )
 }
 
 export function ProductSorterSkeleton() {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams()
   const getActiveSort = () => {
-    const sortParams = searchParams.get("sort");
-    const activeSort = sortOptions.find((sort) => sort.value === sortParams);
-    return activeSort;
-  };
+    const sortParams = searchParams.get("sort")
+    const activeSort = sortOptions.find((sort) => sort.value === sortParams)
+    return activeSort
+  }
   return (
     <Dropdown>
       <DropdownTrigger asChild disabled>
         <Button
           disabled
           variant="defaultOutline"
-          className="h-8 w-8 border-slate-300 dark:border-gray-700 dark:bg-slate-950 bg-white px-3 lg:h-10 lg:w-52 lg:justify-between"
+          className="h-8 w-8 border-slate-300 bg-white px-3 dark:border-gray-700 dark:bg-slate-950 lg:h-10 lg:w-52 lg:justify-between"
         >
           <span className="hidden font-medium lg:block">
             {getActiveSort()?.label ?? "Select Sort"}
@@ -140,5 +140,5 @@ export function ProductSorterSkeleton() {
         </Button>
       </DropdownTrigger>
     </Dropdown>
-  );
+  )
 }
