77085865d47224d5d1097d94dff77f79dcbceaeb
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 19:12:11 +0700

    feat(page): add new checkout page

---
+++ src/app/(user)/(main)/cart/checkout/page.tsx
@@ -0,0 +1,38 @@
+import { BackButton } from "@/components/back-button"
+import { CheckoutPaymentModal } from "@/components/user/cart/checkout-payment-modal"
+import { CheckoutSelectedCartItems } from "@/components/user/cart/checkout-selected-cart-items"
+import { CheckoutSummary } from "@/components/user/cart/checkout-summary"
+import { getUserAddress } from "@/app/actions/user-details"
+
+export const metadata = {
+  title: "Checkout | Next Marketplace",
+}
+
+export default async function CheckoutPage() {
+  const userAddress = await getUserAddress()
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="mb-2 lg:mb-6 lg:px-8">
+        <div className="flex items-center gap-2">
+          <BackButton />
+          <h1 className="text-base font-medium lg:text-xl">Checkout</h1>
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col justify-between gap-4 lg:flex-row">
+        <div className="w-full lg:w-4/6">
+          <div className="mb-4 flex flex-col gap-1 px-4 lg:px-8">
+            <h5 className="text-sm font-medium lg:text-base">
+              Shipping Address
+            </h5>
+            <span className="text-xs text-slate-500 lg:text-sm">
+              {userAddress}
+            </span>
+          </div>
+          <CheckoutSelectedCartItems />
+        </div>
+        <CheckoutSummary />
+      </div>
+      <CheckoutPaymentModal />
+    </div>
+  )
+}
