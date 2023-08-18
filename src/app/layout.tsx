2d213ecec215ad5f5bdee6f264a4922d303c696d
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 18:42:01 +0700

    refactor: add border color for dark mode on toast container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import "./globals.css";
-import "react-toastify/dist/ReactToastify.css";
+import "./globals.css"
+import "react-toastify/dist/ReactToastify.css"
 
-import ClientSessionProvider from "@/components/session-provider";
-import { ThemeClientProvider } from "@/components/theme-client-provider";
-import ToastifyContainer from "@/components/toastify-client-container";
-import { ReactQueryProvider } from "@/lib/react-query-provider";
+import { ReactQueryProvider } from "@/lib/react-query-provider"
+import ClientSessionProvider from "@/components/session-provider"
+import { ThemeClientProvider } from "@/components/theme-client-provider"
+import ToastifyContainer from "@/components/toastify-client-container"
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <ClientSessionProvider>
         <ReactQueryProvider>
-          <body className="bg-slate-50 dark:bg-slate-950 antialiased">
+          <body className="bg-slate-50 antialiased dark:bg-slate-950">
             <ThemeClientProvider>{children}</ThemeClientProvider>
             <ToastifyContainer
               closeButton={false}
@@ -24,12 +24,12 @@ export default function RootLayout({
               hideProgressBar={true}
               newestOnTop={false}
               rtl={false}
-              toastClassName="bg-white  border border-slate-300 rounded-lg"
-              bodyClassName="text-sm font-medium flex gap-4 font-sans text-slate-600"
+              toastClassName="bg-white border border-slate-300 dark:border-gray-700 dark:bg-slate-950 rounded-lg"
+              bodyClassName="text-sm font-medium flex gap-4 font-sans text-slate-600 dark:text-slate-50"
             />
           </body>
         </ReactQueryProvider>
       </ClientSessionProvider>
     </html>
-  );
+  )
 }
