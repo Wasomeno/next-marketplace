b4a53f9d73591e9663e8ac70efc8ead27ef244de
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 19:07:52 +0700

    refactor(tailwind): change border color to gray-800

--- src/components/user/footer.tsx
+++ src/components/user/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="hidden border-t dark:border-gray-700 p-6 lg:block">
+    <footer className="hidden border-t p-6 dark:border-gray-800 lg:block">
       <div className="flex items-center justify-between">
         <div className="">
           <span className="text-base font-medium tracking-wide lg:text-lg">
@@ -15,5 +15,5 @@ export const Footer = () => {
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
