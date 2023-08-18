295e250db3e3b8e1ca3cc8caeef6f9f2111c1955
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 18:45:18 +0700

    refactor(tailwind): change background color to neutral-950

--- src/app/(user)/(main)/layout.tsx
+++ src/app/(user)/(main)/layout.tsx
@@ -1,18 +1,18 @@
-import { Footer } from "@/components/user/footer";
-import { UserMainNavigation } from "@/components/user/navigation/user-main-navigation";
-import { UserMobileNavigation } from "@/components/user/navigation/user-mobile-navigation";
+import { Footer } from "@/components/user/footer"
+import { UserMainNavigation } from "@/components/user/navigation/user-main-navigation"
+import { UserMobileNavigation } from "@/components/user/navigation/user-mobile-navigation"
 
 interface UserLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default async function UserMainLayout({ children }: UserLayoutProps) {
   return (
-    <div className="flex flex-1 flex-col gap-2 bg-white dark:bg-slate-950 lg:gap-6">
+    <div className="flex flex-1 flex-col gap-2 bg-white dark:bg-neutral-950 lg:gap-6">
       <UserMainNavigation />
       {children}
       <Footer />
       <UserMobileNavigation />
     </div>
-  );
+  )
 }
