d961f7292db1fec432444e3d8f2ba2cf0c050c5e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 18:43:10 +0700

    refactor(tailwind): change background color to neutral-950

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,11 +1,11 @@
 interface UserLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default async function UserLayout({ children }: UserLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-slate-950">
+    <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-neutral-950">
       {children}
     </div>
-  );
+  )
 }
