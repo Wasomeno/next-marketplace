7dafdc3a8690b4b2537f5365ddf041817079a851
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-18 18:52:34 +0700

    rafactor(tailwind): remove border-y & change background color to slate-100 and neutral-900 for dark mode

--- src/app/(user)/(main)/categories/[category]/page.tsx
+++ src/app/(user)/(main)/categories/[category]/page.tsx
@@ -1,30 +1,30 @@
-import { Metadata } from "next";
+import { Metadata } from "next"
 
-import { ProductsSection } from "@/components/products-section";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/lib/prisma"
+import { ProductsSection } from "@/components/products-section"
 
 type Props = {
-  params: { category: string };
-  searchParams: { pmin: string; pmax: string; sort: string };
-};
+  params: { category: string }
+  searchParams: { pmin: string; pmax: string; sort: string }
+}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const category = await prisma.category.findUnique({
     where: { slug: params.category },
-  });
+  })
   return {
     title: `${category?.name} | Next Marketplace`,
     description: category?.description,
-  };
+  }
 }
 
 export default async function CategoryProductsPage({
   params,
   searchParams,
 }: Props) {
-  const priceMin = searchParams.pmin;
-  const priceMax = searchParams.pmax;
-  const sort = searchParams.sort ? [searchParams.sort.split(".")] : [];
+  const priceMin = searchParams.pmin
+  const priceMax = searchParams.pmax
+  const sort = searchParams.sort ? [searchParams.sort.split(".")] : []
 
   const category = await prisma.category.findUnique({
     where: { slug: params.category },
@@ -40,16 +40,16 @@ export default async function CategoryProductsPage({
         include: { images: true, category: true },
       },
     },
-  });
+  })
 
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-6">
-      <div className="flex h-36 w-full items-center justify-between border-y bg-slate-50 dark:bg-slate-900 dark:border-y-gray-700 px-10 lg:h-72 lg:px-16 ">
+      <div className="flex h-36 w-full items-center justify-between bg-slate-100 px-10 dark:bg-neutral-900 lg:h-72 lg:px-16 ">
         <h2 className="text-lg font-medium tracking-wider lg:text-4xl">
           {category?.name}
         </h2>
       </div>
       <ProductsSection products={category?.products} />
     </div>
-  );
+  )
 }
