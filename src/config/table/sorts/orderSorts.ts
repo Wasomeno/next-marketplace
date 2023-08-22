8de776350ae247efc9cbad0a4e095e5363e09e94
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-22 20:04:41 +0700

    feat: add getSorts function that returns the sorts option for the new table data sorter component

---
+++ src/config/table/sorts/orderSorts.ts
@@ -0,0 +1,22 @@
+import { Table } from "@tanstack/react-table"
+
+import { TableSort } from "@/components/table-data-sorter"
+
+export function getOrderSorts(table: Table<any>): TableSort[] {
+  return [
+    {
+      id: 1,
+      text: "Id from low to high",
+      onClick() {
+        table.getColumn("id")?.toggleSorting(false)
+      },
+    },
+    {
+      id: 2,
+      text: "Id from high to low",
+      onClick() {
+        table.getColumn("id")?.toggleSorting(true)
+      },
+    },
+  ]
+}
