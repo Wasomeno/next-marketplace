69ed98b4a0fad0613f3892fb9e5181c7cc6bf0af
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-22 19:35:05 +0700

    refactor(page): change the admin orders page to rsc, remove the reducer states and update all the props needed by the modal components

---
+++ src/app/(admin)/admin/(main)/orders/page.tsx
@@ -0,0 +1,23 @@
+import { AdminOrderDetailsModal } from "@/components/admin/admin-order-details-modal"
+import { OrdersTable } from "@/components/admin/order/orders-table"
+import { PageTransitionWrapper } from "@/components/page-transition-wrapper"
+import { getAllOrders } from "@/app/actions/order"
+
+export const metadata = {
+  title: "Admin | Orders",
+}
+
+export default async function AdminOrdersPage() {
+  const orders = await getAllOrders()
+  return (
+    <PageTransitionWrapper className="flex w-full flex-1 flex-col bg-gray-50 p-5 dark:bg-neutral-900">
+      <div className="mb-4 flex items-center justify-between">
+        <h1 className="text-base font-medium tracking-wider lg:text-2xl">
+          Orders
+        </h1>
+      </div>
+      <OrdersTable orders={orders} />
+      <AdminOrderDetailsModal />
+    </PageTransitionWrapper>
+  )
+}
