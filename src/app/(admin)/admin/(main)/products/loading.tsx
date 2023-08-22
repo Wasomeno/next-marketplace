364732bbdfaf6b682603f58912013f40b9215f6f
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-22 19:36:02 +0700

    feat(page): add fallback loading pages for admin categories,orders,& products pages

---
+++ src/app/(admin)/admin/(main)/products/loading.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { ImSpinner7 } from "react-icons/im"
+
+import { PageTransitionWrapper } from "@/components/page-transition-wrapper"
+
+export default function ProductLoadingPage() {
+  return (
+    <PageTransitionWrapper className="flex w-full flex-1 flex-col items-center justify-center bg-gray-50 p-5 dark:bg-neutral-900">
+      <ImSpinner7 className="animate-spin fill-blue-500" size="30" />
+    </PageTransitionWrapper>
+  )
+}
