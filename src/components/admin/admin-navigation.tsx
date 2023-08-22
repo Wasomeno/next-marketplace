bcc4db18a3697dc9f0ad9bdca0de51ca18683bdd
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-22 19:57:10 +0700

    refactor: remove useViewport hook, replace the next js logo with next marketplace logo, change the link active color & add dark mode classes for them

--- src/components/admin/admin-navigation.tsx
+++ src/components/admin/admin-navigation.tsx
@@ -1,29 +1,29 @@
-"use client";
+"use client"
 
-import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { IconType } from "react-icons";
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import clsx from "clsx"
+import { IconType } from "react-icons"
 import {
   MdGridView,
   MdOutlineRequestQuote,
   MdOutlineTableChart,
   MdSpaceDashboard,
-} from "react-icons/md";
-import { twMerge } from "tailwind-merge";
-
-import { useViewport } from "../hooks/useViewport";
+} from "react-icons/md"
+import { twMerge } from "tailwind-merge"
 
 const AdminNavigation = () => {
-  const { width } = useViewport();
-  const path = usePathname();
-
-  if (width < 768) return null;
+  const path = usePathname()
   return (
-    <div className="sticky left-0 top-0 h-screen w-64 border-r bg-white">
+    <div className="sticky left-0 top-0 hidden h-screen w-64 border-r bg-white dark:border-r-neutral-800 dark:bg-neutral-900 lg:block">
       <div className="flex h-20 items-center justify-center gap-2">
-        <Image src="/next.svg" width={90} height={40} alt="shop-logo" />
+        <Image
+          src="/next_marketplace.webp"
+          width={90}
+          height={40}
+          alt="shop-logo"
+        />
       </div>
       <div className="flex flex-col items-center">
         <div className="mb-4 flex w-5/6 flex-col gap-2">
@@ -54,14 +54,14 @@ const AdminNavigation = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 interface NavigationLinkProps {
-  path: string | null;
-  href: string;
-  title: string;
-  Icon: IconType;
+  path: string | null
+  href: string
+  title: string
+  Icon: IconType
 }
 
 const NavigationLink = ({ href, path, title, Icon }: NavigationLinkProps) => {
@@ -71,28 +71,34 @@ const NavigationLink = ({ href, path, title, Icon }: NavigationLinkProps) => {
       className={twMerge(
         clsx(
           "flex items-center gap-4 rounded-md p-4 transition duration-300",
-          path === href && "bg-blue-100"
+          path === href && "bg-blue-500 dark:bg-blue-800"
         )
       )}
     >
       <Icon
         size="25"
         className={twMerge(
-          clsx(path !== href ? "fill-slate-400" : "fill-blue-500")
+          clsx(
+            path !== href
+              ? "fill-slate-400 dark:fill-neutral-500"
+              : "fill-white"
+          )
         )}
       />
       <h5
         className={twMerge(
           clsx(
             "text-sm font-semibold",
-            path !== href ? "text-slate-400" : "text-blue-500"
+            path !== href
+              ? "text-slate-400 dark:text-neutral-500"
+              : "text-white"
           )
         )}
       >
         {title}
       </h5>
     </Link>
-  );
-};
+  )
+}
 
-export default AdminNavigation;
+export default AdminNavigation
