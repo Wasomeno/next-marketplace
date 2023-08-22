a728716a3f9fd0b50045dfefe1428e6f192a1e15
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-22 19:58:44 +0700

    refactor: replace the bell icon with notification dropdown, replace the sun icon with theme switcher component, and replace the dropdown menu with admin main menu

--- src/components/admin/admin-toolbar.tsx
+++ src/components/admin/admin-toolbar.tsx
@@ -1,43 +1,21 @@
-"use client";
-
-import { signOut } from "next-auth/react";
-import React from "react";
-import { BsBell, BsSun } from "react-icons/bs";
-
-import {
-  Dropdown,
-  DropdownContent,
-  DropdownItem,
-  DropdownTrigger,
-} from "../ui/dropdown";
+import { AdminMainMenu } from "./admin-main-menu"
+import { AdminNotificationDropdown } from "./admin-notification-dropdown"
+import { ToolbarThemeSwitcher } from "./toolbar-theme-switcher"
 
 export const AdminToolbar = () => {
   return (
-    <div className="sticky flex w-full items-center justify-between  border-b bg-white px-5 py-2.5">
-      <div className=""></div>
+    <div className="sticky top-0 z-10 flex w-full items-center justify-end  border-b bg-white px-5 py-2.5 dark:border-neutral-700 dark:bg-neutral-900">
       <div className="flex items-center justify-end gap-2.5">
         <div className="flex w-10 justify-center lg:w-12">
-          <BsBell className="h-4 w-4 lg:h-5 lg:w-5" />
+          <AdminNotificationDropdown />
         </div>
         <div className="flex w-10 justify-center lg:w-12">
-          <BsSun className="h-4 w-4 lg:h-5 lg:w-5" />
+          <ToolbarThemeSwitcher />
         </div>
         <div className="flex h-10 w-10 items-center justify-center lg:h-12 lg:w-12">
-          <Dropdown>
-            <DropdownTrigger className="h-8 w-8 rounded-full bg-slate-200 lg:h-10 lg:w-10" />
-            <DropdownContent className="w-40 rounded-md bg-white px-3 py-2.5 shadow-sm">
-              <DropdownItem asChild>
-                <button
-                  onClick={() => signOut({ callbackUrl: "/admin/login" })}
-                  className="flex w-full items-center text-sm hover:ring-0"
-                >
-                  Logout
-                </button>
-              </DropdownItem>
-            </DropdownContent>
-          </Dropdown>
+          <AdminMainMenu />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
