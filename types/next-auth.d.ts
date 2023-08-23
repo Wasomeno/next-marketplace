0752b9e80b6c3e04295d8597a7e21bfca167f444
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-23 21:03:01 +0700

    refactor: add picture value for session and user type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,10 +2,15 @@ import NextAuth, { DefaultSession, DefaultUser } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
-    user: { username?: string; role?: string } & DefaultSession["user"]
+    user: {
+      username?: string
+      role?: string
+      picture?: string
+    } & DefaultSession["user"]
   }
   interface User extends DefaultUser {
     username?: string
     role?: string
+    picture: string
   }
 }
