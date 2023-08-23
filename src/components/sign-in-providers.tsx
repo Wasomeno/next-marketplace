13ee4be6c3fcd501422987bcb0ee4dcd4ca1a9d8
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-23 21:07:50 +0700

    refactor: add active condition when button hovered

--- src/components/sign-in-providers.tsx
+++ src/components/sign-in-providers.tsx
@@ -14,14 +14,14 @@ export const SignInProviders = () => {
         onClick={() =>
           signIn("google", { callbackUrl: "http://localhost:3000" })
         }
-        className="border-slate-300 hover:bg-slate-100 dark:border-gray-800 dark:bg-slate-950"
+        className="border-slate-300 hover:bg-slate-100 active:hover:bg-slate-100 dark:border-gray-800 dark:bg-slate-950 dark:active:hover:bg-slate-900"
       >
         <FcGoogle className="h-6 w-6 lg:h-6 lg:w-6" />
       </Button>
       <Button
         disabled
         variant="defaultOutline"
-        className="border-slate-300 hover:bg-slate-100 dark:border-gray-800 dark:bg-slate-950"
+        className="border-slate-300 active:hover:bg-slate-100 dark:border-gray-800  dark:bg-slate-950 dark:active:hover:bg-slate-900"
       >
         <AiFillGithub className="h-6 w-6 lg:h-6 lg:w-6" />
       </Button>
