38ae406fe4f0e6ef09b740d245c2be50d5edbd07
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-23 20:48:07 +0700

    refactor: change the type for the props to extends from html input element

--- src/components/table-search-input.tsx
+++ src/components/table-search-input.tsx
@@ -1,36 +1,37 @@
 "use client"
 
+import { HTMLInputTypeAttribute } from "react"
 import clsx from "clsx"
 import { RxMagnifyingGlass } from "react-icons/rx"
+import { twMerge } from "tailwind-merge"
 
-import { Input } from "./ui/input"
+import { Input, InputProps } from "./ui/input"
+
+interface TableSearchInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export function TableSearchInput({
-  disabled,
-  onChange = () => {},
-  placeholder,
-}: {
-  disabled?: boolean
-  onChange?: (value: string) => void
-  placeholder?: string
-}) {
+  className,
+  ...props
+}: TableSearchInputProps) {
   return (
     <div
-      className={clsx(
-        "flex h-8 w-40 items-center rounded-md border bg-white p-1.5 dark:border-neutral-600 dark:bg-neutral-900 lg:h-10 lg:w-96",
-        disabled && "opacity-50"
-      )}
+      className={
+        "flex h-8 w-40 items-center rounded-md border bg-white p-1.5 dark:border-neutral-600 dark:bg-neutral-900 lg:h-10 lg:w-96"
+      }
     >
       <div className="flex w-10 items-center justify-center">
         <RxMagnifyingGlass className="text-slate-400 dark:text-white" />
       </div>
       <Input
         type="text"
-        onChange={(event) => {
-          onChange(event.target.value)
-        }}
-        className="h-auto w-full appearance-none border-none p-0 text-xs focus:border-0 focus-visible:ring-0 dark:bg-neutral-900 lg:text-base"
-        placeholder={placeholder}
+        className={twMerge(
+          clsx(
+            "h-auto w-full appearance-none border-none p-0 text-xs focus:border-0 focus-visible:ring-0 dark:bg-neutral-900 lg:text-base",
+            className
+          )
+        )}
+        {...props}
       />
     </div>
   )
