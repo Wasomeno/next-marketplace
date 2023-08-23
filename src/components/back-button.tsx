9313beeaf7aac9f23d78cbe7249910a6cb843b4e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-23 20:37:28 +0700

    refactor: add type for the back button props and wrap it with readonly

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,22 +1,22 @@
-"use client";
+"use client"
 
-import { useRouter } from "next/navigation";
-import { BsArrowLeft } from "react-icons/bs";
+import { useRouter } from "next/navigation"
+import { BsArrowLeft } from "react-icons/bs"
 
-import { Button } from "./ui/button";
+import { PageTransitionWrapper } from "./page-transition-wrapper"
+import { Button } from "./ui/button"
 
-export const BackButton = ({
-  size,
-  text,
-}: {
-  size?: string | number;
-  text?: string;
-}) => {
-  const { back } = useRouter();
+type BackButtonProps = {
+  size?: string | number
+  text?: string
+}
+
+export const BackButton = ({ size, text }: Readonly<BackButtonProps>) => {
+  const { back } = useRouter()
   return (
     <Button onClick={back} className="flex gap-2 bg-transparent p-3 lg:hidden">
       <BsArrowLeft size={size ?? 16} />
       {text && <span className="text-sm font-medium">{text}</span>}
     </Button>
-  );
-};
+  )
+}
