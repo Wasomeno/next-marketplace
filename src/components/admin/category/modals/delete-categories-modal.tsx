fb355618ce872053501ad149ca340c0a728976a4
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-13 20:09:42 +0700

    refactor: change to named function, replace the axios post with deleteCategories server action and change the buttons with Button components

--- src/components/admin/category/modals/delete-categories-modal.tsx
+++ src/components/admin/category/modals/delete-categories-modal.tsx
@@ -1,42 +1,38 @@
 import React, { useRef } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useMutation } from "@tanstack/react-query"
-import axios from "axios"
 import { Id, toast } from "react-toastify"
 
-import { queryClient } from "@/lib/react-query-client"
+import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { deleteCategories } from "@/app/actions/categories"
 
 interface DeleteCategoriesModalProps {
   selectedCategories: Array<number>
 }
 
-export const DeleteCategoriesModal = ({
+export function DeleteCategoriesModal({
   selectedCategories,
-}: DeleteCategoriesModalProps) => {
+}: DeleteCategoriesModalProps) {
   const toastRef = useRef<Id>(0)
   const searchParams = useSearchParams()
   const router = useRouter()
 
   const isDeleteModalOpen = searchParams.get("delete") !== null
-  const deleteCategories = useMutation(
-    () =>
-      axios.post("/api/categories/delete", {
-        categoryIds: selectedCategories,
-      }),
+  const deleteCategoriesMutation = useMutation(
+    async () => await deleteCategories({ categoryIds: selectedCategories }),
     {
       onMutate() {
         toastRef.current = toast.loading("Deleting categories")
         router.push("/admin/categories")
       },
-      onSuccess(response) {
+      onSuccess() {
         toast.update(toastRef.current, {
           type: "success",
-          render: response.data.message,
+          render: "Succesfully Delete Categories",
           isLoading: false,
           autoClose: 1000,
         })
-        queryClient.invalidateQueries(["categories"])
       },
       onError(response: string) {
         toast.update(toastRef.current, {
@@ -61,18 +57,20 @@ export const DeleteCategoriesModal = ({
             </p>
           </div>
           <div className="flex h-20 w-3/6 items-center justify-evenly">
-            <button
-              onClick={() => deleteCategories.mutate()}
-              className="w-20 rounded-lg border py-2.5 text-sm"
+            <Button
+              variant="success"
+              onClick={() => deleteCategoriesMutation.mutate()}
+              className="w-20 rounded-lg py-2.5 text-sm"
             >
               Continue
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="danger"
               onClick={() => router.push("/admin/categories")}
-              className="w-20 rounded-lg border py-2.5 text-sm"
+              className="w-20 rounded-lg py-2.5 text-sm"
             >
               Cancel
-            </button>
+            </Button>
           </div>
         </div>
       </DialogContent>
