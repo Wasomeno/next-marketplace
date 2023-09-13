7d373cec15dd815e0e54ae2d1b6ae22c128d070f
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-13 17:17:32 +0700

    refactor(class): change border class in parent div

--- src/components/admin/admin-toolbar.tsx
+++ src/components/admin/admin-toolbar.tsx
@@ -4,7 +4,7 @@ import { ToolbarThemeSwitcher } from "./toolbar-theme-switcher"
 
 export const AdminToolbar = () => {
   return (
-    <div className="sticky top-0 z-10 flex w-full items-center justify-end  border-b bg-white px-5 py-2.5 dark:border-neutral-700 dark:bg-neutral-900">
+    <div className="sticky top-0 z-10 flex w-full items-center justify-end  border-b bg-white px-5 py-2.5 dark:border-b-neutral-800 dark:bg-neutral-900">
       <div className="flex items-center justify-end gap-2.5">
         <div className="flex w-10 justify-center lg:w-12">
           <AdminNotificationDropdown />
