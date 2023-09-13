bf0098e9d17735dcfb07d250ee3cc735688b5b56
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-13 20:17:37 +0700

    refactor: change the useQuery enabled condition and add a spinner when order details is still fetching

--- src/components/user/order/order-details-modal.tsx
+++ src/components/user/order/order-details-modal.tsx
@@ -6,6 +6,7 @@ import { Product, ProductImage } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { useSession } from "next-auth/react"
+import { FaSpinner } from "react-icons/fa"
 
 import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
 
@@ -19,14 +20,17 @@ export function OrderDetailsModal() {
   const session = useSession()
 
   const orderDetails = useQuery(
-    ["orderDetails"],
+    ["orderDetails", orderId],
     async () => {
       const { data } = await axios.get(
         `/api/users/${session.data?.user?.email}/orders/${orderId}`
       )
       return data
     },
-    { enabled: orderId !== null || session.data?.user?.email !== undefined }
+    {
+      enabled:
+        isViewDetails !== null || session.data?.user?.email !== undefined,
+    }
   )
 
   const date = new Date(orderDetails.data?.created_at)
@@ -41,52 +45,62 @@ export function OrderDetailsModal() {
         className="w-full lg:h-4/6 lg:w-3/6"
       >
         <DialogHeader title="Order Details" />
-        <div className="px-4">
-          <span className="rounded-lg bg-blue-200 px-3 py-2 text-xs font-medium dark:bg-blue-900 lg:text-base">
-            {orderDetails.data?.status.name}
-          </span>
-          <div className="my-4 flex items-center justify-between text-sm">
-            <span className="font-medium">Invoice Number</span>
-            <span>{orderDetails.data?.invoice}</span>
+        {orderDetails.isLoading ? (
+          <div className="flex flex-1 items-center justify-center">
+            <FaSpinner className="animate-spin fill-blue-500" size={30} />
           </div>
-          <div className="flex items-center justify-between text-sm">
-            <span className="font-medium">Transaction Date</span>
-            <span>{date.toDateString()}</span>
-          </div>
-        </div>
-        <div className="mt-6 w-full px-4">
-          <span className="text-sm font-semibold lg:text-base">Products</span>
-          <div className="mt-4 flex flex-col gap-2.5">
-            {orderDetails.data?.products.map(
-              (product: Product & { images: ProductImage[] }) => (
-                <div
-                  key={product.id}
-                  className="flex items-center gap-4 border-t p-4 dark:border-t-neutral-700"
-                >
-                  <div className="flex w-full flex-wrap items-end justify-between gap-2">
-                    <div className="flex w-full gap-2 lg:w-4/6 lg:gap-4">
-                      <div className="relative h-20 w-28 rounded-md bg-slate-300 dark:bg-neutral-400 lg:h-20 lg:w-24">
-                        <Image
-                          src={product.images[0].image_url}
-                          alt="product-image"
-                          fill
-                        />
-                      </div>
-                      <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
-                        <span className="text-sm font-medium">
-                          {product.name}
-                        </span>
-                        <span className="text-sm">
-                          Rp {product.price.toLocaleString("id")}
-                        </span>
+        ) : (
+          <>
+            <div className="px-4">
+              <span className="rounded-lg bg-blue-200 px-3 py-2 text-xs font-medium dark:bg-blue-900 lg:text-sm">
+                {orderDetails.data?.status.name}
+              </span>
+              <div className="my-4 flex items-center justify-between text-sm">
+                <span className="font-medium">Invoice Number</span>
+                <span>{orderDetails.data?.invoice}</span>
+              </div>
+              <div className="flex items-center justify-between text-sm">
+                <span className="font-medium">Transaction Date</span>
+                <span>{date.toDateString()}</span>
+              </div>
+            </div>
+            <div className="mt-6 px-4">
+              <span className="text-sm font-semibold lg:text-base">
+                Products
+              </span>
+              <div className="mt-4 flex flex-col gap-2.5">
+                {orderDetails.data?.products.map(
+                  (product: Product & { images: ProductImage[] }) => (
+                    <div
+                      key={product.id}
+                      className="flex items-center gap-4 border-t p-4 dark:border-t-neutral-700"
+                    >
+                      <div className="flex w-full flex-wrap items-end justify-between gap-2">
+                        <div className="flex w-full gap-2 lg:w-4/6 lg:gap-4">
+                          <div className="relative h-20 w-28 rounded-md bg-slate-300 dark:bg-neutral-400 lg:h-20 lg:w-24">
+                            <Image
+                              src={product.images[0].image_url}
+                              alt="product-image"
+                              fill
+                            />
+                          </div>
+                          <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
+                            <span className="text-sm font-medium">
+                              {product.name}
+                            </span>
+                            <span className="text-sm">
+                              Rp {product.price.toLocaleString("id")}
+                            </span>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              )
-            )}
-          </div>
-        </div>
+                  )
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   )
