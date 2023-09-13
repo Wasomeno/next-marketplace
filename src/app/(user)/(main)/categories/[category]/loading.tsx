1145edcfbb565a0f575d26e13ac741a18dacc73f
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-13 17:35:07 +0700

    refactor: change import for ProductSorterSkeleton

--- src/app/(user)/(main)/categories/[category]/loading.tsx
+++ src/app/(user)/(main)/categories/[category]/loading.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
-import ProductCard from "@/components/user/product-card";
-
-import { ProductSorterSkeleton } from "../../../../../components/product-sorter";
+import { Button } from "@/components/ui/button"
+import { ProductSorterSkeleton } from "@/components/product-sorter"
+import ProductCard from "@/components/user/product-card"
 
 export default function CategoryLoading() {
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-6">
-      <div className="flex h-36 w-full items-center justify-between border-y dark:border-y-gray-700 bg-slate-50 dark:bg-slate-900 px-10 lg:h-72 lg:px-16 ">
-        <div className="h-14 w-72 animate-pulse rounded-md bg-slate-200 dark:bg-slate-800" />
+      <div className="flex h-36 w-full items-center justify-between bg-slate-50 px-10 dark:bg-neutral-900 lg:h-72 lg:px-16 ">
+        <div className="h-14 w-72 animate-pulse rounded-md bg-slate-200 dark:bg-neutral-600" />
       </div>
       <div className="relative flex w-full justify-center gap-10 px-4 lg:px-10">
         <div className="w-full">
@@ -34,5 +33,5 @@ export default function CategoryLoading() {
         </div>
       </div>
     </div>
-  );
+  )
 }
