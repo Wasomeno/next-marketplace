9e11afa3993608a41a12bab8e17c3008e2e13f23
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-26 19:44:24 +0700

    refactor(props): change  table to optional

--- src/config/table/sorts/orderSorts.ts
+++ src/config/table/sorts/orderSorts.ts
@@ -2,20 +2,20 @@ import { Table } from "@tanstack/react-table"
 
 import { TableSort } from "@/components/table-data-sorter"
 
-export function getOrderSorts(table: Table<any>): TableSort[] {
+export function getOrderSorts(table?: Table<any>): TableSort[] {
   return [
     {
       id: 1,
       text: "Id from low to high",
       onClick() {
-        table.getColumn("id")?.toggleSorting(false)
+        table?.getColumn("id")?.toggleSorting(false)
       },
     },
     {
       id: 2,
       text: "Id from high to low",
       onClick() {
-        table.getColumn("id")?.toggleSorting(true)
+        table?.getColumn("id")?.toggleSorting(true)
       },
     },
   ]
