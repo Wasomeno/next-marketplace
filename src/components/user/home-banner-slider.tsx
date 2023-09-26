eafdc9d9ae812f9bd1176f6d918c7a43bb1a4df8
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-26 21:37:03 +0700

    refactor: remove onClick from banner slider parent div & move navigation conditional render to navigation component

--- src/components/user/home-banner-slider.tsx
+++ src/components/user/home-banner-slider.tsx
@@ -17,7 +17,6 @@ export function HomeBannerSlider() {
   return (
     <div
       className="swiper-container relative w-full lg:w-11/12"
-      onClick={() => setShowNavigation(!showNavigation)}
       onMouseEnter={() => setShowNavigation(true)}
       onMouseLeave={() => setShowNavigation(false)}
     >
@@ -55,38 +54,46 @@ export function HomeBannerSlider() {
           />
         </SwiperSlide>
       </SwiperReact>
-      <AnimatePresence>
-        {showNavigation && <HomeBannerSliderNavigation swiperRef={swiperRef} />}
-      </AnimatePresence>
+      <SliderNavigation swiperRef={swiperRef} showNavigation={showNavigation} />
     </div>
   )
 }
 
-const HomeBannerSliderNavigation = ({ swiperRef }: { swiperRef: any }) => {
+const SliderNavigation = ({
+  swiperRef,
+  showNavigation,
+}: {
+  swiperRef: any
+  showNavigation: boolean
+}) => {
   return (
-    <>
-      <motion.button
-        id="btn-prev"
-        onClick={() => swiperRef.current.slidePrev()}
-        initial={{ opacity: 0, bottom: "47%" }}
-        animate={{ opacity: 1, bottom: "50%" }}
-        exit={{ opacity: 0, bottom: "47%" }}
-        transition={{ ease: "easeInOut", duration: 0.2, delay: 0.1 }}
-        className="btn-prev lg: absolute -left-2 z-[5] flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md disabled:opacity-50 dark:bg-slate-800 lg:-left-4 lg:h-12 lg:w-12"
-      >
-        <BsChevronLeft className="h-3 w-3 lg:h-5 lg:w-5" />
-      </motion.button>
-      <motion.button
-        id="btn-next"
-        onClick={() => swiperRef.current.slideNext()}
-        initial={{ opacity: 0, bottom: "47%" }}
-        animate={{ opacity: 1, bottom: "50%" }}
-        exit={{ opacity: 0, bottom: "47%" }}
-        transition={{ ease: "easeInOut", duration: 0.2, delay: 0.1 }}
-        className="btn-next absolute -right-2 z-[5] flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md disabled:opacity-50 dark:bg-slate-800 lg:-right-4 lg:h-12 lg:w-12"
-      >
-        <BsChevronRight className="h-3 w-3 lg:h-5 lg:w-5" />
-      </motion.button>
-    </>
+    <AnimatePresence>
+      {showNavigation && (
+        <>
+          <motion.button
+            id="btn-prev"
+            onClick={() => swiperRef.current.slidePrev()}
+            initial={{ opacity: 0, bottom: "47%" }}
+            animate={{ opacity: 1, bottom: "50%" }}
+            exit={{ opacity: 0, bottom: "47%" }}
+            transition={{ ease: "easeInOut", duration: 0.2, delay: 0.1 }}
+            className="btn-prev lg: absolute -left-2 z-[5] flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md disabled:opacity-50 dark:bg-slate-800 lg:-left-4 lg:h-12 lg:w-12"
+          >
+            <BsChevronLeft className="h-3 w-3 lg:h-5 lg:w-5" />
+          </motion.button>
+          <motion.button
+            id="btn-next"
+            onClick={() => swiperRef.current.slideNext()}
+            initial={{ opacity: 0, bottom: "47%" }}
+            animate={{ opacity: 1, bottom: "50%" }}
+            exit={{ opacity: 0, bottom: "47%" }}
+            transition={{ ease: "easeInOut", duration: 0.2, delay: 0.1 }}
+            className="btn-next absolute -right-2 z-[5] flex h-8 w-8 items-center justify-center rounded-full bg-white shadow-md disabled:opacity-50 dark:bg-slate-800 lg:-right-4 lg:h-12 lg:w-12"
+          >
+            <BsChevronRight className="h-3 w-3 lg:h-5 lg:w-5" />
+          </motion.button>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
