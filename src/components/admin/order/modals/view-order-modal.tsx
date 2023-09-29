91925b03ec9c18aacabb7daba96e043e337f5ced
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-29 21:03:45 +0700

    refactor: rename product to orderProduct

--- src/components/admin/order/modals/view-order-modal.tsx
+++ src/components/admin/order/modals/view-order-modal.tsx
@@ -5,9 +5,8 @@ import Image from "next/image"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useQuery } from "@tanstack/react-query"
 
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog"
 import { getOrder } from "@/app/actions/order"
-
-import { Dialog, DialogContent, DialogHeader } from "../../../ui/dialog"
 
 export const ViewOrderModal = () => {
   const params = useSearchParams()
@@ -52,26 +51,26 @@ export const ViewOrderModal = () => {
         <div className="mt-6 w-full px-4">
           <span className="text-sm font-semibold lg:text-base">Products</span>
           <div className="mt-4 flex flex-col gap-2.5">
-            {orderDetails.data?.products.map((product) => (
+            {orderDetails.data?.products.map((orderProduct) => (
               <div
-                key={product.id}
+                key={orderProduct.id}
                 className="flex items-center gap-4 border-t p-4 dark:border-t-neutral-700"
               >
                 <div className="flex w-full flex-wrap items-end justify-between gap-2">
                   <div className="flex w-full gap-2 lg:w-4/6 lg:gap-4">
                     <div className="relative h-20 w-28 rounded-md bg-slate-300 dark:bg-neutral-400 lg:h-20 lg:w-24">
                       <Image
-                        src={product.images[0].image_url}
-                        alt="product-image"
+                        src={orderProduct.product.images[0].image_url}
+                        alt="orderProduct-image"
                         fill
                       />
                     </div>
                     <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
                       <span className="text-sm font-medium">
-                        {product.name}
+                        {orderProduct.product.name}
                       </span>
                       <span className="text-sm">
-                        Rp {product.price.toLocaleString("id")}
+                        Rp {orderProduct.product.price.toLocaleString("id")}
                       </span>
                     </div>
                   </div>
