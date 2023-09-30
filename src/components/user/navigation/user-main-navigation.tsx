8f656435843e6be08975ae102c7da56ada7fe6ed
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-30 23:16:26 +0700

    refactor: add back button for main navigation and extract the cart button to a separate component

--- src/components/user/navigation/user-main-navigation.tsx
+++ src/components/user/navigation/user-main-navigation.tsx
@@ -1,35 +1,28 @@
 import Image from "next/image"
 import Link from "next/link"
-import { IoCartOutline } from "react-icons/io5"
 
 import { Separator } from "@/components/ui/separator"
-import { getCartItemsCount } from "@/app/actions/cart"
+import { MainNavigationBackButton } from "@/components/user/navigation/main-navigation-back-button"
 
+import { CartButton } from "./cart-button"
 import { ProductSearchInput } from "./product-search-input"
 import { UserMenu } from "./user-menu"
 
 export async function UserMainNavigation() {
-  const cartItemCount = await getCartItemsCount()
   return (
-    <div className="sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b border-b-gray-200 bg-white px-4 shadow-sm dark:border-gray-800 dark:bg-neutral-950">
+    <div className="sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b border-b-gray-200 bg-white px-2 shadow-sm dark:border-gray-800 dark:bg-neutral-950 lg:px-4">
       <div className="flex w-full items-center gap-2 lg:w-3/6">
+        <MainNavigationBackButton size={20} />
         <Link
           href="/"
-          className="relative flex h-12 w-12 justify-center lg:h-16 lg:w-16"
+          className="relative  hidden h-12 w-12 justify-center lg:flex lg:h-16 lg:w-16"
         >
           <Image src="/next_marketplace.webp" alt="next-logo" fill />
         </Link>
         <ProductSearchInput />
       </div>
-      <div className="flex w-3/6 items-center justify-evenly lg:w-44">
-        <Link href="/cart" className="relative font-sans">
-          <IoCartOutline className="h-6 w-6 fill-slate-500 lg:h-7 lg:w-7" />
-          {cartItemCount !== 0 && (
-            <div className="flex-items-center absolute -right-2.5 -top-2 flex h-5 w-5 justify-center rounded-full  border-2 border-white bg-red-600 text-xs font-semibold text-white dark:border-neutral-950 lg:h-5 lg:w-5 lg:text-xs">
-              {cartItemCount}
-            </div>
-          )}
-        </Link>
+      <div className="flex w-3/6 items-center justify-end gap-4 px-2 lg:w-44">
+        <CartButton />
         <Separator
           className="h-7 w-px rounded-full bg-slate-200 dark:bg-gray-700"
           orientation="vertical"
