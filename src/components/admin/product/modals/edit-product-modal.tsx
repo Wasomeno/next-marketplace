14847d3008ae15b5fe4e453ae658aeeb9131ac12
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-01 23:26:12 +0700

    refactor: add a spinner when data is loading, add flex flex-1 flex-col to DialogContent className, move selectedCategory state under product details data and pass product category id as the default state

--- src/components/admin/product/modals/edit-product-modal.tsx
+++ src/components/admin/product/modals/edit-product-modal.tsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { useForm } from "react-hook-form"
+import { FaSpinner } from "react-icons/fa"
 import { toast } from "react-toastify"
 
 import { Button } from "@/components/ui/button"
@@ -18,21 +19,23 @@ import { CategoryPicker } from "../category-picker"
 import { ProductFormData, ProductSchema } from "./add-product-modal"
 
 export function EditProductModal() {
-  const [images, setImages] = useState<Array<FileImage>>([])
-  const [selectedCategory, setSelectedCategory] = useState(0)
-
   const searchParams = useSearchParams()
-  const router = useRouter()
+  const open = searchParams.get("edit") !== null
 
   const productId = parseInt(searchParams.get("id") as string)
-
-  const open = searchParams.get("edit") !== null
 
   const productDetails = useQuery(
     ["productDetails", productId],
     async () => await getProduct(productId),
     { enabled: open }
   )
+
+  const [images, setImages] = useState<Array<FileImage>>([])
+  const [selectedCategory, setSelectedCategory] = useState(
+    productDetails.data?.category.id
+  )
+
+  const router = useRouter()
 
   const { register, getValues, formState, handleSubmit, clearErrors } =
     useForm<ProductFormData>({
@@ -52,7 +55,7 @@ export function EditProductModal() {
         updateProduct({
           ...getValues(),
           productId,
-          categoryId: selectedCategory,
+          categoryId: selectedCategory as number,
         }),
         {
           error: "Error",
@@ -80,7 +83,6 @@ export function EditProductModal() {
 
   useEffect(() => {
     if (!productDetails.isLoading) {
-      setSelectedCategory(productDetails.data?.category_id as number)
       const imageUrls = productDetails.data?.images.map((image) =>
         fetch(image.image_url)
           .then((response) => response.blob())
@@ -102,92 +104,105 @@ export function EditProductModal() {
         clearErrors()
       }}
     >
-      <DialogContent open={open} className="bg-slate-100 lg:h-5/6 lg:w-3/6">
+      <DialogContent
+        open={open}
+        className="flex flex-1 flex-col bg-slate-100 lg:h-5/6 lg:w-3/6"
+      >
         <DialogHeader title="Edit Product" />
-
-        <form
-          onSubmit={handleSubmit(() => updateMutation.mutate())}
-          className="flex flex-1 flex-col gap-4 px-6 py-2"
-        >
-          <div className="flex flex-col items-start gap-1 text-gray-400">
-            <label className="text-sm font-medium text-gray-400">Id</label>
-            <h5 className="text-lg">{productId}</h5>
+        {productDetails.isLoading ? (
+          <div className="flex flex-1 items-center justify-center">
+            <FaSpinner className="animate-spin fill-blue-500" size={30} />
           </div>
-          <div className="flex flex-col items-start gap-1">
-            <label className="text-sm font-medium">Image</label>
-            <ImageUploader files={images} setFiles={setImages} />
-            {formState.errors.name?.message && (
-              <p className="text-sm text-red-600">
-                {formState.errors.name.message}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col items-start gap-1">
-            <label className="text-sm font-medium ">Name</label>
-            <Input
-              {...register("name")}
-              className="dark:border-neutral-600 dark:bg-neutral-800"
-            />
-            {formState.errors.name?.message && (
-              <p className="text-sm text-red-600">
-                {formState.errors.name.message}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col items-start gap-1">
-            <label className="text-sm font-medium">Price</label>
-            <Input
-              type="number"
-              {...register("price", { valueAsNumber: true })}
-              className="dark:border-neutral-600 dark:bg-neutral-800"
-            />
-            {formState.errors.price?.message && (
-              <p className="text-sm text-red-600">
-                {formState.errors.price.message}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col gap-1">
-            <label id="productDescription" className="text-sm">
-              Description
-            </label>
-            <TextArea
-              id="productDescription"
-              className="h-40 dark:border-neutral-600 dark:bg-neutral-800"
-              {...register("description")}
-            />
-            {formState.errors.description?.message && (
-              <p className="text-sm text-red-600">
-                {formState.errors.description.message}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col items-start gap-1">
-            <label className="text-sm font-medium">Stock</label>
-            <Input
-              type="number"
-              {...register("stock", { valueAsNumber: true })}
-              className="dark:border-neutral-600 dark:bg-neutral-800"
-            />
-            {formState.errors.stock?.message && (
-              <p className="text-sm text-red-600">
-                {formState.errors.stock.message}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col items-start gap-1">
-            <label className="text-sm font-medium">Category</label>
-            <CategoryPicker
-              selectedCategory={selectedCategory}
-              selectCategory={(category_id) => setSelectedCategory(category_id)}
-            />
-          </div>
-          <div className="flex items-center justify-center gap-4">
-            <Button variant="success" className="w-40 text-slate-50">
-              Submit
-            </Button>
-          </div>
-        </form>
+        ) : (
+          <form
+            onSubmit={handleSubmit(() => updateMutation.mutate())}
+            className="flex flex-col gap-4 px-6 py-2"
+          >
+            <div className="flex flex-col items-start gap-1 text-gray-400">
+              <label className="text-sm font-medium text-gray-400">Id</label>
+              <h5 className="text-lg">{productId}</h5>
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <label className="text-sm font-medium">Image</label>
+              <ImageUploader files={images} setFiles={setImages} />
+              {formState.errors.name?.message && (
+                <p className="text-sm text-red-600">
+                  {formState.errors.name.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <label className="text-sm font-medium ">Name</label>
+              <Input
+                {...register("name")}
+                className="dark:border-neutral-600 dark:bg-neutral-800"
+              />
+              {formState.errors.name?.message && (
+                <p className="text-sm text-red-600">
+                  {formState.errors.name.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <label className="text-sm font-medium">Price</label>
+              <Input
+                type="number"
+                {...register("price", { valueAsNumber: true })}
+                className="dark:border-neutral-600 dark:bg-neutral-800"
+              />
+              {formState.errors.price?.message && (
+                <p className="text-sm text-red-600">
+                  {formState.errors.price.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-1">
+              <label id="productDescription" className="text-sm">
+                Description
+              </label>
+              <TextArea
+                id="productDescription"
+                className="h-40 dark:border-neutral-600 dark:bg-neutral-800"
+                {...register("description")}
+              />
+              {formState.errors.description?.message && (
+                <p className="text-sm text-red-600">
+                  {formState.errors.description.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <label className="text-sm font-medium">Stock</label>
+              <Input
+                type="number"
+                {...register("stock", { valueAsNumber: true })}
+                className="dark:border-neutral-600 dark:bg-neutral-800"
+              />
+              {formState.errors.stock?.message && (
+                <p className="text-sm text-red-600">
+                  {formState.errors.stock.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <label className="text-sm font-medium">Category</label>
+              <CategoryPicker
+                selectedCategory={
+                  selectedCategory ??
+                  (productDetails.data?.category_id as number)
+                }
+                selectCategory={(category_id) =>
+                  setSelectedCategory(category_id)
+                }
+              />
+            </div>
+            <div className="flex items-center justify-center gap-4">
+              <Button variant="success" className="w-40 text-slate-50">
+                Submit
+              </Button>
+            </div>
+          </form>
+        )}
       </DialogContent>
     </Dialog>
   )
