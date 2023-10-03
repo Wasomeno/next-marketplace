22b96b8fd8de94ed9ba752095ca7aa47dab99df2
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-03 19:43:06 +0700

    refactor: remove unused icons, add conditional render when categories is loading and add state for selected category

--- src/components/product-filter/product-category-filter.tsx
+++ src/components/product-filter/product-category-filter.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Category } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { AnimatePresence, motion } from "framer-motion"
-import { BiChevronRight } from "react-icons/bi"
-import { HiArrowsUpDown } from "react-icons/hi2"
 
 import { getCategories } from "@/app/actions/categories"
 
@@ -26,11 +24,7 @@ export function ProductCategoryFilter() {
 
   const categories = useQuery(["categories"], () => getCategories())
 
-  const activeCategory = categories.data?.find(
-    (category) => category.id.toString() === searchParams.get("category")
-  )
-
-  const [category, setCategory] = useState<Category | undefined>(activeCategory)
+  const [category, setCategory] = useState<Category | undefined>()
 
   function selectCategory(category: Category) {
     setCategory(category)
@@ -40,70 +34,72 @@ export function ProductCategoryFilter() {
     setTimeout(() => router.push(url), 750)
   }
 
+  useEffect(() => {
+    if (searchParams.get("category") !== null && !categories.isLoading) {
+      const activeCategory = categories.data?.find(
+        (category) => category.id.toString() === searchParams.get("category")
+      )
+      setCategory(activeCategory)
+    }
+  }, [categories.isLoading])
+
   return (
     <div>
       <h6 className="mb-2 text-xs font-medium tracking-wide lg:text-sm">
         Categories
       </h6>
-      <Dropdown open={isOpen} onOpenChange={setIsOpen}>
-        <DropdownTrigger asChild>
-          <button className="flex h-8 w-8 items-center justify-center rounded-md border bg-white px-3 text-sm outline-0 dark:border-gray-800 dark:bg-slate-950 lg:h-10 lg:w-52 lg:justify-between">
-            <span className="hidden font-medium lg:block">
+      {categories.isLoading && (
+        <div className="h-10 w-52 animate-pulse rounded-md border bg-slate-200  outline-0 dark:border-gray-800 dark:bg-slate-800 " />
+      )}
+      {!categories.isLoading && (
+        <Dropdown open={isOpen} onOpenChange={setIsOpen}>
+          <DropdownTrigger asChild>
+            <button className="block h-10 w-52 items-center justify-center rounded-md border bg-white px-3 text-start text-xs font-medium outline-0 dark:border-gray-800 dark:bg-slate-950 lg:justify-between lg:text-sm">
               {category?.name ?? "Select Category"}
-            </span>
-            <div className="w-5">
-              <BiChevronRight
-                size="20"
-                className="hidden text-slate-600 dark:text-white lg:block"
-              />
-              <HiArrowsUpDown
-                size="16"
-                className="text-slate-600 dark:text-white lg:hidden"
-              />
-            </div>
-          </button>
-        </DropdownTrigger>
-        <AnimatePresence>
-          {isOpen && (
-            <DropdownContent asChild align="end">
-              <motion.div
-                initial={{ height: "0px" }}
-                animate={{ height: "auto", opacity: 1 }}
-                exit={{ height: "0px" }}
-                className="z-[60] flex w-52 flex-col overflow-hidden rounded-md border bg-white text-sm shadow-sm dark:border-gray-800 dark:bg-slate-950 lg:rounded-b-md lg:rounded-t-none lg:border-t-0"
-              >
-                <DropdownItem asChild>
-                  <button
-                    onClick={() => {
-                      setCategory(undefined)
-                      const newSearchParams = new URLSearchParams(
-                        searchParams.toString()
-                      )
-                      newSearchParams.delete("category")
-                      router.replace(
-                        `${pathname}?${newSearchParams.toString()}`
-                      )
-                    }}
-                    className="px-3 py-2  text-start text-xs font-medium outline-0 ring-0 transition  duration-200 hover:bg-slate-100 hover:dark:bg-slate-800 lg:text-sm"
-                  >
-                    All Category
-                  </button>
-                </DropdownItem>
-                {categories.data?.map((category) => (
-                  <DropdownItem key={category.id} asChild>
+            </button>
+          </DropdownTrigger>
+          <AnimatePresence>
+            {isOpen && (
+              <DropdownContent asChild align="end">
+                <motion.div
+                  initial={{ height: "0px" }}
+                  animate={{ height: "auto", opacity: 1 }}
+                  exit={{ height: "0px" }}
+                  className="z-[60] flex w-52 flex-col overflow-hidden rounded-md border bg-white text-sm shadow-sm dark:border-gray-800 dark:bg-slate-950 lg:rounded-b-md lg:rounded-t-none lg:border-t-0"
+                >
+                  <DropdownItem asChild>
                     <button
-                      onClick={() => selectCategory(category)}
+                      onClick={() => {
+                        setCategory(undefined)
+                        const newSearchParams = new URLSearchParams(
+                          searchParams.toString()
+                        )
+                        newSearchParams.delete("category")
+                        router.replace(
+                          `${pathname}?${newSearchParams.toString()}`
+                        )
+                      }}
                       className="px-3 py-2  text-start text-xs font-medium outline-0 ring-0 transition  duration-200 hover:bg-slate-100 hover:dark:bg-slate-800 lg:text-sm"
                     >
-                      {category.name}
+                      All Category
                     </button>
                   </DropdownItem>
-                ))}
-              </motion.div>
-            </DropdownContent>
-          )}
-        </AnimatePresence>
-      </Dropdown>
+                  {categories.data?.map((category) => (
+                    <DropdownItem key={category.id} asChild>
+                      <button
+                        onClick={() => selectCategory(category)}
+                        className="px-3 py-2  text-start text-xs font-medium outline-0 ring-0 transition  duration-200 hover:bg-slate-100 hover:dark:bg-slate-800 lg:text-sm"
+                      >
+                        {category.name}
+                      </button>
+                    </DropdownItem>
+                  ))}
+                </motion.div>
+              </DropdownContent>
+            )}
+          </AnimatePresence>
+        </Dropdown>
+      )}
     </div>
   )
 }
