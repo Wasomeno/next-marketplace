8039668de02d08566d7dffc6619d31ed0bc1802e
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-03 20:22:13 +0700

    refactor: remove unused api routes

--- src/app/api/users/[user]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-import { prisma } from "@/lib/prisma"
-
-export async function GET(request: Request, context: any) {
-  const { user } = context.params
-  const userDetails = await prisma.user.findUnique({ where: { email: user } })
-  return NextResponse.json(userDetails)
-}
