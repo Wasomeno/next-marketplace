adfc7c2f42c9cdd4f8fd0702960f7bb4f3761ce7
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-05 15:58:16 +0700

    refactor: remove back button

--- src/app/(user)/(main)/cart/checkout/page.tsx
+++ src/app/(user)/(main)/cart/checkout/page.tsx
@@ -12,16 +12,13 @@ export default async function CheckoutPage() {
   const userAddress = await getUserAddress()
   return (
     <div className="flex flex-1 flex-col">
-      <div className="mb-2 lg:mb-6 lg:px-8">
-        <div className="flex items-center gap-2">
-          <BackButton />
-          <h1 className="text-base font-medium lg:text-xl">Checkout</h1>
-        </div>
+      <div className="mb-4 px-4 lg:px-8">
+        <h1 className="text-base font-medium lg:text-xl">Checkout</h1>
       </div>
       <div className="flex flex-1 flex-col justify-between gap-4 lg:flex-row">
         <div className="w-full lg:w-4/6">
           <div className="mb-4 flex flex-col gap-1 px-4 lg:px-8">
-            <h5 className="text-sm font-medium lg:text-base">
+            <h5 className="text-xs font-medium lg:text-base">
               Shipping Address
             </h5>
             <span className="text-xs text-slate-500 lg:text-sm">
