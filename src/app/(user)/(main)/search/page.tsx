4b886c52cca5b4afc15dea7c7f3a1845c9b89707
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-20 23:05:00 +0700

    refactor(search-page): fetch products method

--- src/app/(user)/(main)/search/page.tsx
+++ src/app/(user)/(main)/search/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next"
 
-import { prisma } from "@/lib/prisma"
 import { Products } from "@/components/user/products"
 
 type Props = {
@@ -20,21 +19,5 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function SearchPage({ searchParams }: Props) {
-  const priceMin = searchParams.pmin
-  const priceMax = searchParams.pmax
-  const sort = searchParams.sort ? [searchParams.sort.split(".")] : []
-
-  const products = await prisma.product.findMany({
-    where: {
-      name: { contains: searchParams.q },
-      price: {
-        lte: priceMax ? parseInt(priceMax as string) : 5000000,
-        gte: priceMin ? parseInt(priceMin as string) : 100,
-      },
-    },
-    orderBy: !sort.length ? { price: "asc" } : Object.fromEntries(sort as []),
-
-    include: { categories: true, images: true, reviews: true, store: true },
-  })
-  return <Products products={products} />
+  return <Products />
 }
