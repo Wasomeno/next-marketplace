6d91c00745c9ce07e6029fe370a174b02ff80d69
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-21 01:30:09 +0700

    feat(search-page): add hero section

--- src/app/(user)/(main)/search/page.tsx
+++ src/app/(user)/(main)/search/page.tsx
@@ -19,5 +19,14 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function SearchPage({ searchParams }: Props) {
-  return <Products />
+  return (
+    <div className="flex w-full flex-1 flex-col items-center gap-6">
+      <div className="flex h-36 w-full items-center justify-between bg-slate-100 px-10 lg:h-72 lg:px-16 dark:bg-neutral-900 ">
+        <h2 className="text-lg font-medium tracking-wider lg:text-4xl">
+          You Search for {`"${searchParams.q}"`}
+        </h2>
+      </div>
+      <Products />
+    </div>
+  )
 }
