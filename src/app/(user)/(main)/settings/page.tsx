1a2633b0a04002e17c30004f36071a7a69948e5c
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-20 14:50:49 +0700

    fix(settings-page): layout

--- src/app/(user)/(main)/settings/page.tsx
+++ src/app/(user)/(main)/settings/page.tsx
@@ -10,8 +10,8 @@ export default async function UserSettingsPage() {
   invariant(session)
   return (
     <div className="flex flex-1 flex-col gap-6 rounded-lg border border-gray-200 p-4 shadow-sm lg:flex-row">
-      <div className="flex w-64 flex-col gap-2">
-        <div className="relative h-64 w-full rounded-lg border border-gray-200 bg-gray-50">
+      <div className="flex w-full flex-col items-center gap-2 lg:w-64 lg:items-start">
+        <div className="relative h-64 w-64 rounded-lg border border-gray-200 bg-gray-50">
           <Image
             src={session.user.picture as string}
             alt="user-profile-image"
@@ -24,25 +24,25 @@ export default async function UserSettingsPage() {
       </div>
       <div className="flex flex-col gap-2">
         <h2 className="mb-2 font-medium">User Profile</h2>
-        <div className="flex items-center gap-2 text-sm">
+        <div className="flex flex-col items-start gap-2  text-sm lg:flex-row lg:items-center">
           <span className="w-48">Name</span>
-          <span>{session.user.name}</span>
+          <span className="font-medium">{session.user.name}</span>
         </div>
-        <div className="flex items-center gap-2 text-sm">
+        <div className="flex flex-col items-start gap-2  text-sm lg:flex-row lg:items-center">
           <span className="w-48">Email</span>
-          <span>{session.user.email}</span>
+          <span className="font-medium">{session.user.email}</span>
         </div>
-        <div className="flex items-center gap-2 text-sm">
+        <div className="flex flex-col items-start gap-2  text-sm lg:flex-row lg:items-center">
           <span className="w-48">Phone Number</span>
-          <span>081234-1i4141-8081</span>
+          <span className="font-medium">081234-1i4141-8081</span>
         </div>
-        <div className="flex items-center gap-2 text-sm">
+        <div className="flex flex-col items-start gap-2  text-sm lg:flex-row lg:items-center">
           <span className="w-48">Gender</span>
-          <span>Male</span>
+          <span className="font-medium">Male</span>
         </div>
-        <div className="flex items-center gap-2 text-sm">
+        <div className="flex flex-col items-start gap-2  text-sm lg:flex-row lg:items-center">
           <span className="w-48">Date & Birth</span>
-          <span>03-04-2000</span>
+          <span className="font-medium">03-04-2000</span>
         </div>
       </div>
     </div>
