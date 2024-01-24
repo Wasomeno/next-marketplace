32947b0c876bdf0b25cb030978f63b51950d2462
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-24 14:33:10 +0700

    fix(categories-action): add filter and sort props for getCategories action

--- src/app/actions/categories.ts
+++ src/app/actions/categories.ts
@@ -1,13 +1,19 @@
-import { Prisma } from "@prisma/client"
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
 
-import { prisma } from "@/lib/prisma"
+type GetCategoriesProps = {
+  search?: string
+  sort?: Record<string, "desc" | "asc">
+}
 
-export async function getCategories(): Promise<
+export async function getCategories(props?: GetCategoriesProps): Promise<
   Prisma.CategoryGetPayload<{
     include: { _count: { select: { products: true } }; images: true }
   }>[]
 > {
   const categories = await prisma.category.findMany({
+    orderBy: props?.sort,
+    where: { name: { contains: props?.search } },
     include: { _count: { select: { products: true } }, images: true },
   })
   return categories
