233a66aa6d38e6d83c8d4abdeda5170d7100bb0e
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-24 14:34:51 +0700

    feat(user-store-orders): add filter and sort searchParams props

--- src/app/(user)/(store)/store/(main)/orders/page.tsx
+++ src/app/(user)/(store)/store/(main)/orders/page.tsx
@@ -2,8 +2,21 @@ import { ViewOrderModal } from "@/components/admin/order/modals/view-order-modal
 import { OrderTable } from "@/components/admin/order/order-table"
 import { getOrders } from "@/app/actions/user/order"
 
-export default async function UserStoreOrders() {
-  const orders = await getOrders()
+type OrderPageProps = {
+  searchParams: {
+    sort?: string
+    search?: string
+  }
+}
+
+export default async function UserStoreOrders(props: OrderPageProps) {
+  const { sort, search } = props.searchParams
+  const sortOrders: Record<string, "asc" | "desc"> = sort
+    ? {
+        [sort.split(".")[0]]: sort.split(".")[1] as "asc" | "desc",
+      }
+    : { id: "desc" }
+  const orders = await getOrders({ sort: sortOrders, search })
   return (
     <div className="flex flex-1 flex-col gap-2 lg:gap-4">
       <h1 className="text-lg font-medium lg:text-2xl">Orders</h1>
