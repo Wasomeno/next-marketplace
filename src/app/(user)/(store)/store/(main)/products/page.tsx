10b20a5e7d763c6abadaf8500b9e2995fd80ed3e
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-25 19:55:53 +0700

    refactor(store-products-page): change to client component

--- src/app/(user)/(store)/store/(main)/products/page.tsx
+++ src/app/(user)/(store)/store/(main)/products/page.tsx
@@ -1,8 +1,10 @@
-import { Metadata } from "next"
-import invariant from "tiny-invariant"
+"use client"
 
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+
+import { queryClient } from "@/lib/react-query-client"
 import { ProductTable } from "@/components/admin/product/product-table"
-import { getStore, getStoreProducts } from "@/app/actions/store/store"
+import { getStoreProducts } from "@/app/actions/store/store"
 
 type UserStoreProductsProps = {
   searchParams: {
@@ -13,27 +15,13 @@ type UserStoreProductsProps = {
   }
 }
 
-export async function generateMetadata(): Promise<Metadata> {
-  const store = await getStore()
-  return {
-    title: `Products | ${store?.name}`,
-  }
-}
-
-export default async function UserStoreProducts(props: UserStoreProductsProps) {
-  const { status, search, sort } = props.searchParams
-  const sortProduct: Record<string, "asc" | "desc"> = sort
-    ? { [sort.split(".")[0]]: sort.split(".")[1] as "asc" | "desc" }
-    : { id: "asc" }
-
-  const products = await getStoreProducts({ status, search, sort: sortProduct })
-
-  invariant(products)
-
+export default function UserStoreProducts(props: UserStoreProductsProps) {
   return (
     <div className="flex flex-1 flex-col gap-2">
       <h1 className="text-lg font-medium lg:text-2xl">Products</h1>
-      <ProductTable products={products} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ProductTable />
+      </HydrationBoundary>
     </div>
   )
 }
