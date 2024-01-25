5b9286ff4edcef246a0ec951efd474c7c857798e
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-25 19:59:58 +0700

    chore(react-query-provider):

--- src/lib/react-query-provider.tsx
+++ src/lib/react-query-provider.tsx
@@ -1,12 +1,13 @@
-"use client";
+"use client"
 
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./react-query-client";
+import { QueryClientProvider } from "@tanstack/react-query"
+
+import { queryClient } from "./react-query-client"
 
 export const ReactQueryProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-};
+  )
+}
