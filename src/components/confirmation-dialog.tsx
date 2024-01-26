7b252acac8bca2e1fc786e58363dd3a6fae88142
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-26 22:40:51 +0700

    fix(confirmation-dialog): how to implement the dialog component

--- src/components/confirmation-dialog.tsx
+++ src/components/confirmation-dialog.tsx
@@ -1,7 +1,8 @@
 import React from "react"
+import { AnimatePresence } from "framer-motion"
 
 import { Button } from "./ui/button"
-import { Dialog, DialogContent } from "./ui/dialog"
+import { Dialog, DialogContent, DialogOverlay, DialogPortal } from "./ui/dialog"
 
 type ConfirmationDialogProps = {
   open: boolean
@@ -20,35 +21,42 @@ export function ConfirmationDialog({
 }: ConfirmationDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent
-        open={open}
-        className="flex h-fit flex-col justify-between lg:w-96"
-      >
-        <div className="border-b border-gray-300 px-4 py-2">
-          <span className="font-medium">Are you sure ?</span>
-        </div>
-        <div className="p-4">
-          <p>{body}</p>
-        </div>
-        <div className="flex w-full items-center justify-evenly gap-2 px-4 pb-2">
-          <Button
-            variant="success"
-            size="sm"
-            onClick={onConfirm}
-            className="w-full rounded-lg border py-2.5 text-xs font-medium text-white"
-          >
-            Confirm
-          </Button>
-          <Button
-            variant="danger"
-            size="sm"
-            onClick={onCancel}
-            className="w-full rounded-lg border py-2.5 text-xs font-medium text-white"
-          >
-            Cancel
-          </Button>
-        </div>
-      </DialogContent>
+      <AnimatePresence>
+        {open && (
+          <DialogPortal forceMount>
+            <DialogOverlay />
+            <DialogContent
+              open={open}
+              className="flex h-48 flex-col justify-between lg:h-fit lg:w-96"
+            >
+              <div className="border-b border-gray-300 px-4 py-2">
+                <span className="font-medium">Are you sure ?</span>
+              </div>
+              <div className="p-4">
+                <p>{body}</p>
+              </div>
+              <div className="flex w-full items-center justify-evenly gap-2 px-4 pb-2">
+                <Button
+                  variant="success"
+                  size="sm"
+                  onClick={onConfirm}
+                  className="w-full rounded-lg border py-2.5 text-xs font-medium text-white"
+                >
+                  Confirm
+                </Button>
+                <Button
+                  variant="danger"
+                  size="sm"
+                  onClick={onCancel}
+                  className="w-full rounded-lg border py-2.5 text-xs font-medium text-white"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </DialogContent>
+          </DialogPortal>
+        )}
+      </AnimatePresence>
     </Dialog>
   )
 }
