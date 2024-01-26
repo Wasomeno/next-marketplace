0d39fa7e9fd42e24e9e5560517cd7465a8c4b5ba
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-26 17:46:43 +0700

    refactor(store-orders-page): remove view order modal

--- src/app/(user)/(store)/store/(main)/orders/page.tsx
+++ src/app/(user)/(store)/store/(main)/orders/page.tsx
@@ -1,4 +1,3 @@
-import { ViewOrderModal } from "@/components/admin/order/modals/view-order-modal"
 import { OrderTable } from "@/components/admin/order/order-table"
 import { getOrders } from "@/app/actions/user/order"
 
@@ -21,7 +20,6 @@ export default async function UserStoreOrders(props: OrderPageProps) {
     <div className="flex flex-1 flex-col gap-2 lg:gap-4">
       <h1 className="text-lg font-medium lg:text-2xl">Orders</h1>
       <OrderTable orders={orders} />
-      <ViewOrderModal />
     </div>
   )
 }
