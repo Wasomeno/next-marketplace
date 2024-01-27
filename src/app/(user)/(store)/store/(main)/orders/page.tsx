ce0c10395b89bf74d029758761548a0da23ca29e
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-28 01:59:49 +0700

    refactor(store-orders-page): replace with getStoreOrders

--- src/app/(user)/(store)/store/(main)/orders/page.tsx
+++ src/app/(user)/(store)/store/(main)/orders/page.tsx
@@ -1,5 +1,5 @@
 import { OrderTable } from "@/components/admin/order/order-table"
-import { getOrders } from "@/app/actions/user/order"
+import { getStoreOrders } from "@/app/actions/store/order"
 
 type OrderPageProps = {
   searchParams: {
@@ -15,7 +15,8 @@ export default async function UserStoreOrders(props: OrderPageProps) {
         [sort.split(".")[0]]: sort.split(".")[1] as "asc" | "desc",
       }
     : { id: "desc" }
-  const orders = await getOrders({ sort: sortOrders, search })
+  const orders = await getStoreOrders()
+
   return (
     <div className="flex flex-1 flex-col gap-2 lg:gap-4">
       <h1 className="text-lg font-medium lg:text-2xl">Orders</h1>
