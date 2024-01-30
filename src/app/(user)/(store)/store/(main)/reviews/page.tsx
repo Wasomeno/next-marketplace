8e6fcfc7268095a2d4803233c83ec0b00049dbed
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-30 11:21:45 +0700

    style(store-reviews-page): add gap-2 for viewport lower than large

--- src/app/(user)/(store)/store/(main)/reviews/page.tsx
+++ src/app/(user)/(store)/store/(main)/reviews/page.tsx
@@ -2,7 +2,7 @@ import { StoreProductReviewsTable } from "@/components/user/store/store-product-
 
 export default async function UserStoreReviews() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-2 lg:gap-4">
       <h1 className="text-lg font-medium lg:text-2xl">Reviews</h1>
       <StoreProductReviewsTable />
     </div>
