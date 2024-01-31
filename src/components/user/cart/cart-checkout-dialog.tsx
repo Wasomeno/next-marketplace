a39ef4b3941a979476f72dab4927e9ed6b107138
Wasomeno <kevinanandapradipta@gmail.com>
2024-01-31 21:58:26 +0700

    style(cart-checkout-dialog): border color

--- src/components/user/cart/cart-checkout-dialog.tsx
+++ src/components/user/cart/cart-checkout-dialog.tsx
@@ -20,8 +20,8 @@ export const CartCheckoutDialog = ({
   )
 
   return (
-    <div className="sticky bottom-0 w-full bg-white dark:bg-neutral-950 lg:w-2/6 lg:px-8">
-      <div className="border-t border-slate-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-md lg:border lg:border-slate-300 lg:p-4 lg:shadow-none lg:dark:border-gray-800">
+    <div className="sticky bottom-0 w-full bg-white lg:w-2/6 lg:px-8 dark:bg-neutral-950">
+      <div className="border-t border-gray-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] lg:rounded-md lg:border lg:border-gray-200 lg:p-4 lg:shadow-none dark:border-t-gray-800 lg:dark:border-gray-800">
         <span className="hidden text-xs lg:inline lg:text-lg">
           Cart Summary
         </span>
@@ -41,7 +41,7 @@ export const CartCheckoutDialog = ({
             )
             router.push("/cart/checkout")
           }}
-          className="my-1 w-full rounded-lg bg-blue-400 py-3  text-xs font-medium text-white dark:bg-blue-900 lg:text-sm"
+          className="my-1 w-full rounded-lg bg-blue-400 py-3  text-xs font-medium text-white lg:text-sm dark:bg-blue-900"
         >
           Checkout
         </Button>
