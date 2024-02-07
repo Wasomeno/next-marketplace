b2ad08e3808a69c7914c02d22ec888c28f25257f
Wasomeno <kevinanandapradipta@gmail.com>
2024-02-07 16:48:08 +0700

    refactor(store-layout): remove store

--- src/app/(user)/(store)/store/(main)/layout.tsx
+++ src/app/(user)/(store)/store/(main)/layout.tsx
@@ -10,12 +10,11 @@ export default async function StoreLayout({
 }: {
   children: ReactNode
 }) {
-  const store = await getStore()
   return (
     <div className="flex max-h-screen flex-1 flex-col dark:bg-neutral-950">
       <StoreTopNavigation />
       <div className="flex flex-1 flex-col lg:flex-row">
-        <StoreSideNavigation store={store} />
+        <StoreSideNavigation />
         <div className="flex flex-1 flex-col p-4 lg:p-6">{children}</div>
       </div>
       <StoreMobileNavigation />
