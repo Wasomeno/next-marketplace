19ba8ce022888bbb1fef6f95364370464777655d
Wasomeno <kevinanandapradipta@gmail.com>
2024-02-10 03:55:07 +0700

    refactor(categories-page): add description and image

--- src/app/(user)/(main)/categories/[category]/page.tsx
+++ src/app/(user)/(main)/categories/[category]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Image from "next/image"
 
 import { prisma } from "@/lib/prisma"
 import { Products } from "@/components/user/products"
@@ -21,14 +22,27 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function CategoryProductsPage({ params }: Props) {
   const category = await prisma.category.findUnique({
     where: { slug: params.category },
+    include: { images: true },
   })
 
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-6">
-      <div className="flex h-36 w-full items-center justify-between bg-slate-100 px-10 lg:h-72 lg:px-16 dark:bg-neutral-900 ">
-        <h2 className="text-lg font-medium tracking-wider lg:text-4xl">
-          {category?.name}
-        </h2>
+      <div className="flex h-36 w-full items-center gap-6 bg-gradient-to-r from-blue-300 to-sky-100 px-6 dark:bg-neutral-900 lg:h-72 lg:px-16 ">
+        <div className="relative h-24 w-24 overflow-hidden  rounded-full  shadow-md lg:h-36 lg:w-36">
+          <Image
+            src={category?.images[0].url as string}
+            alt={category?.images[0].name as string}
+            fill
+          />
+        </div>
+        <div className="space-y-1 lg:space-y-2">
+          <h2 className="text-lg font-medium tracking-wider lg:text-4xl">
+            {category?.name}
+          </h2>
+          <p className="text-ellipsis text-sm tracking-wide lg:text-base">
+            {category?.description}
+          </p>
+        </div>
       </div>
       <Products />
     </div>
