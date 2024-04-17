928111e32a64176fb15782d881dd1326c4f2a25f
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-17 19:04:16 +0700

    refactor(admin-toolbar): add use client

--- src/modules/admin/common/components/admin-toolbar.tsx
+++ src/modules/admin/common/components/admin-toolbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ThemeSwitcher } from "@/components/theme-switcher"
 
 import { AdminMainMenu } from "./admin-main-menu"
@@ -6,9 +8,7 @@ export const AdminToolbar = () => {
   return (
     <div className="sticky top-0 z-10 flex w-full items-center justify-end  border-b bg-white px-5 py-2.5 dark:border-b-neutral-800 dark:bg-neutral-900">
       <div className="flex items-center justify-end gap-2.5">
-        <div className="flex w-10 justify-center lg:w-12">
-          <ThemeSwitcher />
-        </div>
+        <ThemeSwitcher />
         <div className="flex h-10 w-10 items-center justify-center lg:h-12 lg:w-12">
           <AdminMainMenu />
         </div>
