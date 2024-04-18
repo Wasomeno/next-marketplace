01a8f80d0828273ca006de0b7042b11ebf476323
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-18 21:56:59 +0700

    refactor: transaction list card

--- src/modules/user/store/dashboard-page/components/recent-transaction-list.tsx
+++ src/modules/user/store/dashboard-page/components/recent-transaction-list.tsx
@@ -3,8 +3,8 @@
 import React from "react"
 import { getStoreInvoices } from "@/actions/store/invoice"
 import { useQuery } from "@tanstack/react-query"
+import moment from "moment"
 import { IoList } from "react-icons/io5"
-import { LiaStackpath } from "react-icons/lia"
 
 import { NoData } from "@/components/no-data"
 import { Skeleton } from "@/components/skeleton"
@@ -24,16 +24,28 @@ const RecentTransactionList = () => {
   return (
     <ol className="space-y-2">
       {transactions.isLoading && skeletons}
-
       {!transactions.isLoading &&
         transactions.data &&
         (transactions.data.length > 0 ? (
           transactions.data?.map((transaction) => (
-            <div
-              key={transaction.id}
-              className="space-y-2 rounded-lg px-4 py-2 shadow-sm"
-            >
-              {transaction.id}
+            <div className="space-y-2 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+              <div className="flex flex-wrap items-center justify-between border-b  border-b-gray-200 bg-gray-50 px-4 py-2">
+                <span className="text-sm font-medium">{transaction.id}</span>
+                <span className="text-sm font-medium text-gray-400">
+                  {moment(transaction.created_at).format("LLLL")}
+                </span>
+              </div>
+              <div className="p-4">
+                <div className="flex flex-wrap justify-between gap-4 pb-0">
+                  <div className="space-y-2">
+                    {transaction.products.length} Items
+                  </div>
+                  <div className="flex items-center gap-4 pt-0">
+                    <span className="text-sm font-medium">Total</span>
+                    <span>Rp. {transaction.total.toLocaleString("id")}</span>
+                  </div>
+                </div>
+              </div>
             </div>
           ))
         ) : (
