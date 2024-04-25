aa43a35c00df4bc1885617a01cd02a12d6849d36
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-25 16:10:39 +0700

    style(user-cart-page): remove margin bottom from title

--- src/app/(user)/(main)/cart/page.tsx
+++ src/app/(user)/(main)/cart/page.tsx
@@ -20,7 +20,7 @@ export default async function Cart() {
   return (
     <div className="flex flex-1 flex-col">
       <div className="mb-4 px-4 lg:px-8">
-        <h1 className="mb-2 text-base font-medium lg:text-xl">Cart</h1>
+        <h1 className="text-base font-medium lg:text-xl">Cart</h1>
         <span className="font-sans text-xs font-medium text-slate-400 lg:text-sm">
           {cart.count} items
         </span>
