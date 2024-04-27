a712caed59f2e1cfb0966ca87c7a5ad0df091e47
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-27 19:50:52 +0700

    style(input): add border color when focus

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={twMerge(
           clsx(
-            "flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:focus-visible:outline-none",
+            "flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus:border-gray-400 focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:focus-visible:outline-none",
             className
           )
         )}
