bb91961e7c779b7adb12964b894083ea5bb2c2ef
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-29 02:00:52 +0700

    refactor(cart-summary): change layout

--- src/modules/user/cart-page/components/cart-summary.tsx
+++ src/modules/user/cart-page/components/cart-summary.tsx
@@ -19,18 +19,15 @@ export const CartSummary = ({ selectedItems }: CartCheckoutDialogProps) => {
 
   return (
     <div className="sticky bottom-0 w-full bg-white dark:bg-neutral-950 lg:w-2/6 lg:px-8">
-      <div className="border-t border-gray-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-md lg:border lg:border-gray-200 lg:p-4 lg:shadow-none lg:dark:border-gray-800">
-        <span className="hidden text-xs lg:inline lg:text-lg">
-          Cart Summary
-        </span>
-        <div className="my-2 flex justify-between">
-          <span className="text-xs text-slate-500 lg:text-base">Subtotal</span>
-          <span className="text-sm lg:text-lg">
+      <div className="border-t border-gray-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-md lg:border lg:border-gray-200 lg:p-4 lg:shadow-sm lg:dark:border-gray-800">
+        <h5 className="hidden text-xs lg:inline lg:text-base">Cart Summary</h5>
+        <div className="mb-4 mt-2 flex justify-between">
+          <span className="text-xs text-slate-500 lg:text-sm">Subtotal</span>
+          <span className="text-sm lg:text-base">
             Rp {total.toLocaleString("id")}
           </span>
         </div>
         <Button
-          variant="default"
           disabled={!selectedItems.length}
           onClick={() => {
             localStorage.setItem(
@@ -39,7 +36,7 @@ export const CartSummary = ({ selectedItems }: CartCheckoutDialogProps) => {
             )
             router.push("/cart/checkout")
           }}
-          className="my-1 w-full rounded-lg bg-blue-400 py-3  text-xs font-medium text-white dark:bg-blue-900 lg:text-sm"
+          className="w-full disabled:hover:bg-gray-100 lg:text-xs"
         >
           Checkout
         </Button>
