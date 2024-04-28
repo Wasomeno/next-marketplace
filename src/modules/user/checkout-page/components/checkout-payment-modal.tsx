5c14b9c81f802a6a4ed8df579e29add5319dcc4c
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-29 05:50:05 +0700

    refactor(checkout-payment-modal): implement useMutation for the checkout function, add component for error condition, change the layout

--- src/modules/user/checkout-page/components/checkout-payment-modal.tsx
+++ src/modules/user/checkout-page/components/checkout-payment-modal.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter, useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { checkout } from "@/actions/user/order"
 import { Prisma } from "@prisma/client"
+import { useMutation } from "@tanstack/react-query"
 import { AnimatePresence } from "framer-motion"
 import Lottie from "lottie-react"
 import { IconType } from "react-icons"
 import { BsBank, BsCreditCard2Front } from "react-icons/bs"
+import { FaCircleXmark } from "react-icons/fa6"
 import { ImSpinner8 } from "react-icons/im"
 import { MdMoneyOffCsred } from "react-icons/md"
 
@@ -27,6 +29,8 @@ type PaymentMethod = {
   name: string
   Icon: IconType
 }
+
+type PaymentStatus = "idle" | "loading" | "error" | "success"
 
 const paymentMethods: PaymentMethod[] = [
   {
@@ -48,9 +52,12 @@ const paymentMethods: PaymentMethod[] = [
 
 export function CheckoutPaymentModal({ address }: { address: string }) {
   const [selectedMethod, setSelectedMethod] = useState(0)
-  const [status, setStatus] = useState("idle")
-  const searchParams = useSearchParams()
+  const [status, setStatus] = useState<PaymentStatus>("idle")
+
   const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
   const isOpen = searchParams.get("payment") !== null
 
   const selectedCartItems: Prisma.CartItemGetPayload<{
@@ -62,11 +69,38 @@ export function CheckoutPaymentModal({ address }: { address: string }) {
     0
   )
 
+  function onOpenChange(isOpen: boolean) {
+    const urlSearchParams = new URLSearchParams(searchParams)
+    if (isOpen) {
+      urlSearchParams.set("payment", "true")
+
+      router.replace(`${pathname}?${urlSearchParams.toString()}`, {
+        scroll: true,
+      })
+    } else {
+      urlSearchParams.delete("payment")
+      router.replace(`${pathname}?${urlSearchParams.toString()}`, {
+        scroll: true,
+      })
+      setStatus("idle")
+    }
+  }
+
+  const checkoutMutation = useMutation({
+    mutationFn: async () => {
+      await checkout({
+        total,
+        cartItems: selectedCartItems,
+        address,
+      })
+    },
+    onMutate: () => setStatus("loading"),
+    onSuccess: () => setStatus("success"),
+    onError: () => setStatus("error"),
+  })
+
   return (
-    <Dialog
-      open={isOpen}
-      onOpenChange={() => status !== "loading" && router.push("/cart/checkout")}
-    >
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <AnimatePresence>
         {isOpen && (
           <DialogPortal>
@@ -79,60 +113,53 @@ export function CheckoutPaymentModal({ address }: { address: string }) {
                 <>
                   <DialogHeader title="Payment Details" />
                   <div className="flex flex-1 flex-col">
-                    <div className="h-full px-4 lg:px-6">
-                      <h5 className="text-sm font-medium tracking-wide">
+                    <div className="h-full px-6">
+                      <h5 className="text-sm font-medium tracking-wide lg:text-base">
                         Payment Methods
                       </h5>
                       <div className="mt-2 flex w-full flex-col gap-2">
                         {paymentMethods.map((method) => (
                           <Button
+                            variant="defaultOutline"
                             onClick={() => {
                               setSelectedMethod((current) =>
                                 current === method.id ? 0 : method.id
                               )
                             }}
                             key={method.id}
-                            className="rounded-mdr flex items-center justify-between bg-gray-50 shadow-sm dark:border-neutral-700 dark:bg-neutral-800"
+                            className="flex items-center justify-between shadow-sm"
                           >
                             <div className="flex items-center gap-4">
                               <method.Icon
                                 className="text-slate-400"
                                 size="18"
                               />
-                              <span className="text-sm">{method.name}</span>
+                              <span className="text-sm font-normal">
+                                {method.name}
+                              </span>
                             </div>
                             <Input
                               checked={selectedMethod === method.id}
                               type="radio"
-                              className="form h-3 w-3"
+                              className="form h-3 w-3 accent-black"
                             />
                           </Button>
                         ))}
                       </div>
                     </div>
                     <div className="border-t p-4 dark:border-t-neutral-700">
-                      <div className="mb-2">
-                        <h5 className="text-sm font-medium tracking-wide">
+                      <div className="mb-2 flex items-center justify-between">
+                        <span className="text-sm font-medium tracking-wide lg:text-base">
                           Total
-                        </h5>
-                        <span className="text-base lg:text-lg">
+                        </span>
+                        <span className="text-sm lg:text-base">
                           Rp. {total.toLocaleString("id")}
                         </span>
                       </div>
                       <Button
-                        variant="default"
                         disabled={!selectedMethod}
-                        onClick={async () => {
-                          setStatus("loading")
-                          const date = new Date()
-                          await checkout({
-                            total,
-                            cartItems: selectedCartItems,
-                            address,
-                          })
-                          setStatus("success")
-                        }}
-                        className="w-full bg-blue-400 font-semibold text-white dark:bg-blue-900"
+                        onClick={() => checkoutMutation.mutate()}
+                        className="w-full"
                       >
                         Confirm Payment
                       </Button>
@@ -142,10 +169,7 @@ export function CheckoutPaymentModal({ address }: { address: string }) {
               )}
               {status === "loading" && (
                 <div className="flex flex-1 flex-col items-center justify-center gap-4">
-                  <ImSpinner8
-                    size="40"
-                    className="animate-spin text-blue-500 dark:text-blue-800"
-                  />
+                  <ImSpinner8 size="40" className="animate-spin" />
                   <span className="w-3/6 text-center text-sm font-medium lg:text-base">
                     Confirming Payment and Finishing Your Order
                   </span>
@@ -156,17 +180,35 @@ export function CheckoutPaymentModal({ address }: { address: string }) {
                   <Lottie
                     animationData={checkMarkSuccess}
                     loop={false}
-                    className="h-24 w-24"
+                    className="h-20 w-20 lg:h-24 lg:w-24"
                   />
-                  <span className="text-center text-sm font-medium lg:text-base">
-                    Payment & Order Confirmed
+                  <span className=" text-center text-sm font-medium lg:text-base">
+                    Thank You!
+                  </span>
+                  <span className=" text-center text-sm font-medium lg:text-base">
+                    Your payment was received successfully
                   </span>
                   <Button
                     onClick={() => router.push("/orders")}
                     variant="defaultOutline"
                     className="mt-4 border border-slate-300 text-xs dark:border-gray-800"
                   >
-                    Go to Your Orders
+                    View your orders
+                  </Button>
+                </div>
+              )}
+              {status === "error" && (
+                <div className="flex flex-1 flex-col items-center justify-center gap-2">
+                  <FaCircleXmark className="h-10 w-10 text-red-600" />
+                  <span className="text-center text-sm font-medium lg:text-base">
+                    Error When Proccessing Your Payment
+                  </span>
+                  <Button
+                    variant="defaultOutline"
+                    className="mt-2 border border-slate-300 text-xs dark:border-gray-800"
+                    onClick={() => checkoutMutation.mutate()}
+                  >
+                    Retry
                   </Button>
                 </div>
               )}
