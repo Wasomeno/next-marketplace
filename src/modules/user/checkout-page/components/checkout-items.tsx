99dedf6e6126a79d63c00b04c5f886a0561cc5eb
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-29 05:48:52 +0700

    refactor(checkout-items): replace selectedCartItems value with a state, change the layout

--- src/modules/user/checkout-page/components/checkout-items.tsx
+++ src/modules/user/checkout-page/components/checkout-items.tsx
@@ -1,38 +1,47 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import { Prisma } from "@prisma/client"
 
+type CheckoutItem = Prisma.CartItemGetPayload<{
+  include: { product: { include: { images: true } } }
+}>
+
 export function CheckoutItems() {
-  const selectedCartItems: Prisma.CartItemGetPayload<{
-    include: { product: { include: { images: true } } }
-  }>[] = JSON.parse(localStorage?.getItem("selectedCartItems") as string)
+  const [selectedCartItems, setSelectedCartItems] =
+    useState<Array<CheckoutItem>>()
+
+  useEffect(() => {
+    if (window !== undefined) {
+      setSelectedCartItems(
+        JSON.parse(window.localStorage.getItem("selectedCartItems") as string)
+      )
+    }
+  }, [])
 
   return (
-    <div className="flex flex-1 flex-col gap-2 px-4 lg:px-8">
-      {selectedCartItems.map((selectedCartItem) => (
-        <div
-          key={selectedCartItem.id}
-          className="flex items-center gap-4 border-t p-4 dark:border-t-gray-800"
-        >
-          <div className="flex w-full flex-wrap items-end justify-between gap-2">
-            <div className="flex w-full gap-2 lg:w-4/6 lg:gap-4">
-              <div className="relative h-12 w-16 rounded-md bg-slate-300 dark:bg-neutral-400 lg:h-20 lg:w-24">
-                <Image
-                  src={selectedCartItem.product.featured_image_url}
-                  alt="product-image"
-                  fill
-                />
-              </div>
-              <div className="flex w-4/6 flex-col gap-1 lg:w-3/6">
-                <span className="text-xs font-medium lg:text-sm">
-                  {selectedCartItem.product.name}
-                </span>
-                <span className="text-xs lg:text-sm">
-                  Rp {selectedCartItem.product.price.toLocaleString("id")} x{" "}
-                  {selectedCartItem.amount}
-                </span>
-              </div>
+    <div className="flex flex-1 flex-col gap-4">
+      {selectedCartItems?.map((selectedCartItem) => (
+        <div className="flex flex-1 gap-4">
+          <div className="relative h-20 w-20 overflow-hidden rounded-lg border lg:h-24 lg:w-24">
+            <Image
+              src={selectedCartItem.product.featured_image_url}
+              fill
+              alt={selectedCartItem.product.name}
+            />
+          </div>
+          <div className="flex flex-1 flex-col justify-between lg:flex-row">
+            <span className="inline w-full text-sm lg:w-fit">
+              {selectedCartItem.product.name}
+            </span>
+            <div className="flex w-full flex-col gap-2 lg:w-fit lg:items-end">
+              <h5 className="text-sm">
+                Rp. {selectedCartItem.product.price.toLocaleString("id")}
+              </h5>
+              <h5 className="text-sm text-gray-500">
+                Amount: {selectedCartItem.amount}
+              </h5>
             </div>
           </div>
         </div>
