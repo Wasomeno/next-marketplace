3c28105ba3e8fc2c0fdd35c771ea3505cbfc6ca0
Wasomeno <kevinanandapradipta@gmail.com>
2024-04-29 06:00:14 +0700

    refactor(checkout-summary): change layout, implement more viable  open checkout payment modal method

--- src/modules/user/checkout-page/components/checkout-summary.tsx
+++ src/modules/user/checkout-page/components/checkout-summary.tsx
@@ -1,39 +1,53 @@
 "use client"
 
-import { useRouter } from "next/navigation"
-import { Prisma } from "@prisma/client"
+import { useEffect, useState } from "react"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 
+import { CheckoutItem } from "./checkout-items"
+
 export function CheckoutSummary() {
+  const [selectedCartItems, setSelectedCartItems] =
+    useState<Array<CheckoutItem>>()
   const router = useRouter()
 
-  const selectedCartItems: Prisma.CartItemGetPayload<{
-    include: { product: { include: { images: true } } }
-  }>[] = JSON.parse(localStorage?.getItem("selectedCartItems") as string)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
-  const total = selectedCartItems.reduce(
-    (prev, current) => (prev += current.amount * current.product.price),
-    0
-  )
+  const total =
+    selectedCartItems?.reduce(
+      (prev, current) => (prev += current.amount * current.product.price),
+      0
+    ) ?? 0
+
+  function openPaymentModal() {
+    const urlSearchParams = new URLSearchParams(searchParams)
+    urlSearchParams.set("payment", "true")
+    router.replace(`${pathname}?${urlSearchParams.toString()}`)
+  }
+
+  useEffect(() => {
+    if (typeof localStorage !== "undefined") {
+      setSelectedCartItems(
+        JSON.parse(localStorage.getItem("selectedCartItems") as string)
+      )
+    }
+  }, [])
 
   return (
-    <div className="sticky bottom-0 w-full lg:w-2/6 lg:px-8">
+    <div className="sticky bottom-0 w-full bg-white lg:w-2/6 lg:px-8">
       <div className="border-t border-gray-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-md  lg:border lg:p-4 lg:shadow-none lg:dark:border-gray-800">
-        <span className="hidden text-xs lg:inline lg:text-lg">
+        <span className="hidden text-xs font-medium lg:inline lg:text-base">
           Order Summary
         </span>
         <div className="my-2 flex justify-between">
-          <span className="text-xs text-slate-500 lg:text-base">Subtotal</span>
-          <span className="text-sm lg:text-lg">
+          <span className="text-sm text-slate-500 lg:text-base">Subtotal</span>
+          <span className="text-sm lg:text-base">
             Rp {total.toLocaleString("id")}
           </span>
         </div>
-        <Button
-          variant="default"
-          onClick={() => router.push("/cart/checkout?payment=true")}
-          className="my-1 w-full rounded-lg bg-blue-400 py-3 text-xs  font-medium text-white hover:bg-blue-300 dark:bg-blue-900 lg:text-sm"
-        >
+        <Button onClick={openPaymentModal} className="w-full lg:text-xs">
           Checkout
         </Button>
       </div>
