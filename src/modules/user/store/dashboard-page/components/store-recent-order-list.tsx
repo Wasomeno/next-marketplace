447c489af7c9c8ff1881ce1d55640d6f7c3fc2d0
Wasomeno <kevinanandapradipta@gmail.com>
2024-05-04 16:03:11 +0700

    refactor(store-recent-order-list): chnage layout to table

--- src/modules/user/store/dashboard-page/components/store-recent-order-list.tsx
+++ src/modules/user/store/dashboard-page/components/store-recent-order-list.tsx
@@ -2,12 +2,37 @@
 
 import React from "react"
 import { getStoreOrders } from "@/actions/user/order"
+import { Order, Prisma } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
+import { ColumnDef } from "@tanstack/react-table"
 import moment from "moment"
-import { IoList } from "react-icons/io5"
 
-import { NoData } from "@/components/no-data"
-import { Skeleton } from "@/components/skeleton"
+import { DataTable } from "@/components/data-table"
+
+const columns: ColumnDef<
+  Prisma.OrderGetPayload<{ include: { products: true } }>
+>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+    enableColumnFilter: false,
+  },
+  {
+    accessorFn: ({ products }) => products?.length,
+    header: "Item Amount",
+    enableColumnFilter: false,
+  },
+  {
+    accessorFn: ({ total }) => `Rp. ${total?.toLocaleString("id")}`,
+    header: "Total",
+    enableColumnFilter: false,
+  },
+  {
+    accessorFn: ({ created_at }) => moment(created_at).format("LLLL"),
+    header: "Ordered at",
+    enableColumnFilter: false,
+  },
+]
 
 export const StoreRecentOrderList: React.FC<{ storeId: number }> = ({
   storeId,
@@ -17,50 +42,13 @@ export const StoreRecentOrderList: React.FC<{ storeId: number }> = ({
     queryFn: () => getStoreOrders({ storeId }),
   })
 
-  const skeletons = Array(5)
-    .fill("")
-    .map((_, index) => (
-      <Skeleton key={index} className="h-12 w-full lg:w-1/2" />
-    ))
-
   return (
-    <ol className="space-y-2">
-      {transactions.isLoading && skeletons}
-      {!transactions.isLoading &&
-        transactions.data &&
-        (transactions.data.length > 0 ? (
-          transactions.data?.map((transaction) => (
-            <div className="space-y-2 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
-              <div className="flex flex-wrap items-center justify-between border-b  border-b-gray-200 bg-gray-50 px-4 py-2">
-                <span className="text-sm font-medium">{transaction.id}</span>
-                <span className="text-sm font-medium text-gray-400">
-                  {moment(transaction.created_at).format("LLLL")}
-                </span>
-              </div>
-              <div className="p-4">
-                <div className="flex flex-wrap justify-between gap-4 pb-0">
-                  <div className="space-y-2">
-                    {transaction.products.length} Items
-                  </div>
-                  <div className="flex items-center gap-4 pt-0">
-                    <span className="text-sm font-medium">Total</span>
-                    <span>Rp. {transaction.total.toLocaleString("id")}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="flex h-72 items-center justify-center">
-            <NoData text="No Recent Transaction" icon={<IoList size={24} />} />
-          </div>
-        ))}
-
-      {!transactions.isLoading && !transactions.data && (
-        <div className="flex h-72 items-center justify-center">
-          <NoData text="No Recent Transaction" icon={<IoList size={24} />} />
-        </div>
-      )}
-    </ol>
+    <div className="space-y-2 rounded-lg border p-4 lg:px-6 lg:py-4">
+      <h2 className="text-base font-medium lg:text-lg">Recent Transactions</h2>
+      <DataTable
+        data={transactions.data ?? Array(5).fill("")}
+        columns={columns}
+      />
+    </div>
   )
 }
