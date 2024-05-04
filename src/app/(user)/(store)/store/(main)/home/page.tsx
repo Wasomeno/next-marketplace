1a74fc681046f6d4d1a289d71c555a6990805b76
Wasomeno <kevinanandapradipta@gmail.com>
2024-05-04 16:02:38 +0700

    feat(user-store-home-page): add store sales and order count data

--- src/app/(user)/(store)/store/(main)/home/page.tsx
+++ src/app/(user)/(store)/store/(main)/home/page.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { Metadata } from "next"
 import { redirect } from "next/navigation"
+import { getStoreSales } from "@/actions/store/store"
 import { getCachedSession } from "@/actions/store/user"
+import { getStoreOrderCount } from "@/actions/user/order"
 import { getUserStore } from "@/actions/user/user-details"
 import { AnalyticCard } from "@/modules/user/store/dashboard-page/components/analytic-card"
 import { StoreRecentOrderList } from "@/modules/user/store/dashboard-page/components/store-recent-order-list"
@@ -26,11 +28,17 @@ export default async function UserStorePage() {
     redirect("/")
   }
 
+  const sales = await getStoreSales()
+
+  const orderAmount = await getStoreOrderCount({ storeId: store.id })
+
   return (
     <div className="flex flex-col gap-4 lg:gap-6">
-      <div className="space-y-2">
+      <div className="lg:space-y-2">
         <h1 className="text-lg font-medium lg:text-2xl">Dashboard</h1>
-        <p className="text-gray-500">Monitor your store performance</p>
+        <p className="text-xs text-gray-500 lg:text-base">
+          Monitor your store performance
+        </p>
       </div>
       <div className="wrap grid w-full grid-cols-2 gap-4 lg:h-24 lg:grid-cols-3">
         <AnalyticCard
@@ -38,23 +46,19 @@ export default async function UserStorePage() {
           icon={
             <LiaMoneyBillSolid className=" h-8 w-8 text-green-700 lg:h-10 lg:w-10" />
           }
-          data={`Rp. 10000`}
+          data={`Rp. ${sales.toLocaleString("id")}`}
         />
         <AnalyticCard
           title="Orders"
           icon={
             <FaFileInvoiceDollar className="h-8 w-8 text-blue-700 lg:h-10 lg:w-10" />
           }
-          data={10}
+          data={orderAmount}
         />
       </div>
       <StoreSalesChart />
-      <div className="space-y-2">
-        <h2 className="text-base font-medium lg:text-lg">
-          Recent Transactions
-        </h2>
-        <StoreRecentOrderList storeId={store.id} />
-      </div>
+
+      <StoreRecentOrderList storeId={store.id} />
     </div>
   )
 }
