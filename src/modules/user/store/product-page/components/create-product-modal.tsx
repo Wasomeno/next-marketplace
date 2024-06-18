bda876aaddb7282aecca54010600060349c911ac
Wasomeno <kevinanandapradipta@gmail.com>
2024-06-18 08:55:10 +0700

    refactor(create-product-modal): replace dialog with reponsive dialog

--- src/modules/user/store/product-page/components/create-product-modal.tsx
+++ src/modules/user/store/product-page/components/create-product-modal.tsx
@@ -1,36 +1,41 @@
 "use client"
 
-import React from "react"
-import { usePathname, useRouter } from "next/navigation"
-import { getCategories } from "@/actions/categories"
-import { addProduct } from "@/actions/store/products"
-import { categoryQueryKeys } from "@/modules/user/common/queryKeys/categoryQueryKeys"
-import { storeQueryKeys } from "@/modules/user/common/queryKeys/storeQueryKeys"
-import { useSearchParamsValues } from "@/utils"
-import { useUploadThing } from "@/utils/uploadthing"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { AnimatePresence } from "framer-motion"
-import { useForm } from "react-hook-form"
-import { ImSpinner8 } from "react-icons/im"
-import { toast } from "sonner"
-import * as z from "zod"
+import { AnimatePresence } from "framer-motion";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { ImSpinner8 } from "react-icons/im";
+import { toast } from "sonner";
+import * as z from "zod";
 
-import { Button } from "@/components/ui/button"
+import { getCategories } from "@/actions/categories";
+import { addProduct } from "@/actions/store/products";
+import { ImageUploader } from "@/components/image-uploader";
+import { MultiSelectDropdown } from "@/components/multi-select-dropdown";
+import {
+  ResponsiveDialog,
+  ResponsiveDialogContent,
+  ResponsiveDialogHeader
+} from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogOverlay,
-  DialogPortal,
-} from "@/components/ui/dialog"
-import { Fieldset } from "@/components/ui/fieldset"
-import { Input } from "@/components/ui/input"
-import { TextArea } from "@/components/ui/text-area"
-import { ImageUploader } from "@/components/image-uploader"
-import { MultiSelectDropdown } from "@/components/multi-select-dropdown"
+  DialogPortal
+} from "@/components/ui/dialog";
+import { Fieldset } from "@/components/ui/fieldset";
+import { Input } from "@/components/ui/input";
+import { TextArea } from "@/components/ui/text-area";
+import { categoryQueryKeys } from "@/modules/user/common/queryKeys/categoryQueryKeys";
+import { storeQueryKeys } from "@/modules/user/common/queryKeys/storeQueryKeys";
+import { useSearchParamsValues } from "@/utils";
+import { useUploadThing } from "@/utils/uploadthing";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { TBaseDataFilterParams } from "../../../../../../types"
+import { TBaseDataFilterParams } from "../../../../../../types";
 
 export const ProductSchema = z.object({
   images: z
@@ -142,122 +147,120 @@ export const CreateProductModal: React.FC<{ storeId: number }> = ({
   }
 
   return (
-    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+    <ResponsiveDialog open={isOpen} onOpenChange={onOpenChange}>
       <AnimatePresence>
-        {isOpen && (
-          <DialogPortal forceMount>
-            <DialogOverlay />
-            <DialogContent open={isOpen} className="h-[36rem] lg:w-[30rem]">
-              <DialogHeader
-                title="Create Store Product"
-                description="Make a new product for your store"
+        <ResponsiveDialogContent
+          open={isOpen}
+          className="h-[36rem] lg:w-[30rem]"
+        >
+          <ResponsiveDialogHeader
+            title="Create Store Product"
+            description="Make a new product for your store"
+          />
+          <form
+            onSubmit={form.handleSubmit((formData) =>
+              createProductMutation.mutate(formData)
+            )}
+            className="flex w-full flex-col gap-4 overflow-scroll p-4 px-4 lg:px-6"
+          >
+            <Fieldset
+              label="Images"
+              className="flex flex-col gap-2 "
+              error={form.formState.errors.images}
+            >
+              <ImageUploader
+                mode="multiple"
+                onImagesChange={(images) =>
+                  form.setValue("images", images, { shouldValidate: true })
+                }
               />
-              <form
-                onSubmit={form.handleSubmit((formData) =>
-                  createProductMutation.mutate(formData)
-                )}
-                className="flex w-full flex-col gap-4 p-4 px-4 lg:px-6"
+            </Fieldset>
+            <Fieldset
+              label="Name"
+              className="flex flex-col gap-2 "
+              error={form.formState.errors.name}
+            >
+              <Input
+                placeholder="Input your product name"
+                className="w-full"
+                {...form.register("name")}
+              />
+            </Fieldset>
+            <Fieldset
+              label="Categories"
+              className="flex flex-col gap-2 "
+              error={form.formState.errors.categoryIds}
+            >
+              <MultiSelectDropdown
+                options={categoryOptions ?? []}
+                onOptionsChange={(options) => {
+                  form.setValue(
+                    "categoryIds",
+                    options.map((option) => Number(option.value))
+                  )
+                }}
+              />
+            </Fieldset>
+            <Fieldset
+              label="Price"
+              className="flex flex-col gap-2 "
+              error={form.formState.errors.price}
+            >
+              <Input
+                type="number"
+                placeholder="Input your product price"
+                className="w-full"
+                {...form.register("price", { valueAsNumber: true })}
+              />
+            </Fieldset>
+            <Fieldset
+              label="Stock"
+              className="flex flex-col gap-2 "
+              error={form.formState.errors.stock}
+            >
+              <Input
+                type="number"
+                placeholder="Input your product stock"
+                className="w-full"
+                {...form.register("stock", { valueAsNumber: true })}
+              />
+            </Fieldset>
+            <Fieldset
+              label="Description"
+              className="col-span-2 flex flex-col gap-2 "
+              error={form.formState.errors.description}
+            >
+              <TextArea
+                className="h-36 w-full"
+                placeholder="Input your product description"
+                {...form.register("description")}
+              />
+            </Fieldset>
+            <div className="flex flex-wrap-reverse items-center justify-end gap-2">
+              <Button
+                type="button"
+                variant="defaultOutline"
+                size="sm"
+                className="w-full lg:w-32 lg:text-xs"
+                onClick={() => router.replace(pathname)}
               >
-                <Fieldset
-                  label="Images"
-                  className="flex flex-col gap-2 "
-                  error={form.formState.errors.images}
-                >
-                  <ImageUploader
-                    mode="multiple"
-                    onImagesChange={(images) =>
-                      form.setValue("images", images, { shouldValidate: true })
-                    }
-                  />
-                </Fieldset>
-                <Fieldset
-                  label="Name"
-                  className="flex flex-col gap-2 "
-                  error={form.formState.errors.name}
-                >
-                  <Input
-                    placeholder="Input your product name"
-                    className="w-full"
-                    {...form.register("name")}
-                  />
-                </Fieldset>
-                <Fieldset
-                  label="Categories"
-                  className="flex flex-col gap-2 "
-                  error={form.formState.errors.categoryIds}
-                >
-                  <MultiSelectDropdown
-                    options={categoryOptions ?? []}
-                    onOptionsChange={(options) => {
-                      form.setValue(
-                        "categoryIds",
-                        options.map((option) => Number(option.value))
-                      )
-                    }}
-                  />
-                </Fieldset>
-                <Fieldset
-                  label="Price"
-                  className="flex flex-col gap-2 "
-                  error={form.formState.errors.price}
-                >
-                  <Input
-                    type="number"
-                    placeholder="Input your product price"
-                    className="w-full"
-                    {...form.register("price", { valueAsNumber: true })}
-                  />
-                </Fieldset>
-                <Fieldset
-                  label="Stock"
-                  className="flex flex-col gap-2 "
-                  error={form.formState.errors.stock}
-                >
-                  <Input
-                    type="number"
-                    placeholder="Input your product stock"
-                    className="w-full"
-                    {...form.register("stock", { valueAsNumber: true })}
-                  />
-                </Fieldset>
-                <Fieldset
-                  label="Description"
-                  className="col-span-2 flex flex-col gap-2 "
-                  error={form.formState.errors.description}
-                >
-                  <TextArea
-                    className="h-36 w-full"
-                    placeholder="Input your product description"
-                    {...form.register("description")}
-                  />
-                </Fieldset>
-                <div className="flex flex-wrap-reverse items-center justify-end gap-2">
-                  <Button
-                    type="button"
-                    variant="defaultOutline"
-                    size="sm"
-                    className="w-full lg:w-32 lg:text-xs"
-                    onClick={() => router.replace(pathname)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    variant="default"
-                    size="sm"
-                    className="w-full lg:w-32 lg:text-xs"
-                    disabled={createProductMutation.isPending}
-                  >
-                    {createProductMutation.isPending && (
-                      <ImSpinner8 className="animate-spin" />
-                    )}
-                    Submit
-                  </Button>
-                </div>
-              </form>
-            </DialogContent>
-          </DialogPortal>
-        )}
+                Cancel
+              </Button>
+              <Button
+                variant="default"
+                size="sm"
+                className="w-full lg:w-32 lg:text-xs"
+                disabled={createProductMutation.isPending}
+              >
+                {createProductMutation.isPending && (
+                  <ImSpinner8 className="animate-spin" />
+                )}
+                Submit
+              </Button>
+            </div>
+          </form>
+        </ResponsiveDialogContent>
       </AnimatePresence>
-    </Dialog>
+    </ResponsiveDialog>
   )
 }
