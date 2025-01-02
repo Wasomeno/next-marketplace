eea950e0a357410d57f8cdad69c0d2bdd630cd49
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-02 16:30:54 +0700

    refactor(user/product/add-to-cart): remove sticky behavior

--- src/app/(user)/(main)/[storeSlug]/[productSlug]/_components/add-to-cart-form.tsx
+++ src/app/(user)/(main)/[storeSlug]/[productSlug]/_components/add-to-cart-form.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { addToCart } from "@/actions/user/cart"
 import { Product } from "@prisma/client"
 import { useMutation } from "@tanstack/react-query"
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 import { ImSpinner8 } from "react-icons/im"
 import { toast } from "sonner"
 
@@ -39,52 +39,54 @@ export const AddToCartForm = ({ product }: AddToCartFormProps) => {
   })
 
   return (
-    <div className="sticky bottom-0 m-0 h-fit w-screen border-t border-slate-200 bg-white p-2 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:top-20 lg:mr-20 lg:w-80 lg:rounded-lg lg:border lg:border-slate-200 lg:p-4 lg:shadow-sm lg:dark:border-gray-800">
-      <span className="hidden lg:inline-block">Add to Cart Details</span>
-      <div className="hidden lg:block">
-        <div className="my-4 flex items-center justify-between gap-4">
-          <span className="text-xs text-slate-500 lg:text-sm">Quantity</span>
-          <div className="relative flex w-fit items-center justify-center gap-4 overflow-hidden rounded-md border border-gray-200 text-sm font-medium dark:bg-slate-800">
-            <button
-              onClick={decrement}
-              className="border-r border-gray-200 bg-gray-50 lg:h-6 lg:w-6"
-            >
-              -
-            </button>
-            <div className="w-4 text-center">
-              <span>{amount}</span>
+    <div className="bottom-0 m-0 h-fit w-full space-y-4 mb-6 border-t border-slate-200 bg-white p-2 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-lg lg:border lg:border-slate-200 lg:p-4 lg:shadow-sm lg:dark:border-gray-800">
+      <span className="hidden lg:inline-block">Add to Cart</span>
+      <div className="space-y-3">
+        <div className="hidden lg:block">
+          <div className="flex items-center justify-between gap-4">
+            <span className="text-xs text-slate-500 lg:text-sm">Quantity</span>
+            <div className="relative flex w-fit items-center justify-center gap-4 overflow-hidden rounded-md border border-gray-200 text-sm font-medium dark:bg-slate-800">
+              <button
+                onClick={decrement}
+                className="border-r border-gray-200 bg-gray-50 lg:h-6 lg:w-6"
+              >
+                -
+              </button>
+              <div className="w-4 text-center">
+                <span>{amount}</span>
+              </div>
+              <button
+                onClick={increment}
+                className="border-l border-gray-200 bg-gray-50 lg:h-6 lg:w-6"
+              >
+                +
+              </button>
             </div>
-            <button
-              onClick={increment}
-              className="border-l border-gray-200 bg-gray-50 lg:h-6 lg:w-6"
-            >
-              +
-            </button>
           </div>
         </div>
-      </div>
-      <div className="my-2 hidden items-center justify-between lg:flex">
-        <span className="text-xs text-slate-500 lg:text-sm">Subtotal</span>
-        <span className="text-sm lg:text-base">
-          Rp. {(amount * product.price).toLocaleString("id")}
-        </span>
-      </div>
-      <div className="flex w-full gap-2 lg:flex-col lg:gap-0">
-        <Button
-          variant="default"
-          disabled={addToCartMutation.isPending}
-          className="w-full"
-          onClick={() =>
-            sessionStatus === "unauthenticated"
-              ? router.push("/login")
-              : addToCartMutation.mutate()
-          }
-        >
-          {addToCartMutation.isPending && (
-            <ImSpinner8 className="animate-spin" />
-          )}
-          Add to Cart
-        </Button>
+        <div className="hidden items-center justify-between lg:flex">
+          <span className="text-xs text-slate-500 lg:text-sm">Subtotal</span>
+          <span className="text-sm lg:text-base">
+            Rp. {(amount * product.price).toLocaleString("id")}
+          </span>
+        </div>
+        <div className="flex w-full gap-2 lg:flex-col lg:gap-0">
+          <Button
+            variant="default"
+            disabled={addToCartMutation.isPending}
+            className="w-full"
+            onClick={() =>
+              sessionStatus === "unauthenticated"
+                ? router.push("/login")
+                : addToCartMutation.mutate()
+            }
+          >
+            {addToCartMutation.isPending && (
+              <ImSpinner8 className="animate-spin" />
+            )}
+            Add to Cart
+          </Button>
+        </div>
       </div>
     </div>
   )
