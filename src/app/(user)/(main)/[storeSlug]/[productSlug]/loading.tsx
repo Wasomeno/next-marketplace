20a532f707885286f3533a8178f2e36e808dde20
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-05 17:28:22 +0700

    style(product/loading-page): skeleton responsive style

--- src/app/(user)/(main)/[storeSlug]/[productSlug]/loading.tsx
+++ src/app/(user)/(main)/[storeSlug]/[productSlug]/loading.tsx
@@ -1,5 +1,4 @@
 import { Skeleton } from "@/components/skeleton"
-import { Separator } from "@/components/ui/separator"
 import { WishListButton } from "./_components"
 
 export default function ProductPageLoading() {
@@ -7,7 +6,7 @@ export default function ProductPageLoading() {
     <div className="relative flex flex-1 flex-col gap-10 lg:flex-row">
       <div className="flex-1 space-y-10 mx-24 my-4">
         <div className="flex flex-1 flex-wrap justify-center gap-10 lg:flex-nowrap lg:justify-normal">
-          <Skeleton className="w-full lg:w-[45rem] lg:h-[45rem]" />
+          <Skeleton className="w-full lg:w-[45rem] h-80 lg:h-[50rem]" />
           <div className="flex flex-1 flex-col">
             <div className="mb-2 flex items-center justify-between">
               <Skeleton className="lg:h-[36px] w-52" />
