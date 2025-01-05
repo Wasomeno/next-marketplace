4a2eec180b23cfde80c1780eea0c0bd1d3779de0
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-05 23:03:20 +0700

    feat(store): add store dashboard header dropdown component

---
+++ src/app/(user)/(store)/store/(main)/home/_components/store-dashboard-header-dropdown.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { DatePicker, DateRange } from "@/components/ui/date-picker"
+import { useSearchParamsValues } from "@/utils"
+import { format } from "date-fns"
+import { usePathname, useRouter } from "next/navigation"
+
+export const StoreDashboardHeaderDropdown = () => {
+  const searchParamValues = useSearchParamsValues<{
+    month: string
+    year: string
+  }>()
+
+  const pathname = usePathname()
+  const router = useRouter()
+
+  function onSelectDate(date: Date | Date[] | DateRange | null) {
+    // const searchParams = new URLSearchParams(searchParamValues)
+    // if (date) {
+    //   searchParams.set("date", format(date, "m"))
+    //   searchParams.set("month", format(date, "m"))
+    //   searchParams.set("year", format(date, "yyyy"))
+    // } else {
+    //   searchParams.delete("month")
+    //   searchParams.delete("year")
+    // }
+    // router.replace(`${pathname}?${searchParams.toString()}`)
+    console.log(date)
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      <DatePicker mode="range" onChange={onSelectDate} />
+    </div>
+  )
+}
