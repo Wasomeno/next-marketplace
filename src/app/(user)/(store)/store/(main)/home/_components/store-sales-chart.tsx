af544b6f7751da1025b62b08087b1d412a0b684a
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-05 23:02:01 +0700

    refactor(store/store-sales-chart): remove year and month dropdown

--- src/app/(user)/(store)/store/(main)/home/_components/store-sales-chart.tsx
+++ src/app/(user)/(store)/store/(main)/home/_components/store-sales-chart.tsx
@@ -2,9 +2,7 @@
 
 import { useSearchParamsValues } from "@/utils"
 import { useQuery } from "@tanstack/react-query"
-import clsx from "clsx"
 import moment from "moment"
-import { usePathname, useRouter } from "next/navigation"
 import { useState } from "react"
 import {
   Area,
@@ -15,27 +13,7 @@ import {
   XAxis,
 } from "recharts"
 
-import { Dropdown, Option } from "@/components/dropdown"
-import { Button } from "@/components/ui/button"
 import { getStoreMonthlySales, getStoreYearlySales } from "../../../_actions"
-
-function getMonthOptions() {
-  const monthOptions: Option[] = []
-  for (let i = 0; i < 12; i++) {
-    const time = moment().set("month", i)
-    monthOptions.push({ label: time.format("MMMM"), value: i })
-  }
-  return monthOptions
-}
-
-function getYearOptions() {
-  const yearOptions: Option[] = []
-  for (let i = 0; i <= 10; i++) {
-    const time = moment().add("year", i)
-    yearOptions.push({ label: time.format("YYYY"), value: time.format("YYYY") })
-  }
-  return yearOptions
-}
 
 export const StoreSalesChart = () => {
   const [chartMode, setChartMode] = useState<"month" | "year">("month")
@@ -45,8 +23,6 @@ export const StoreSalesChart = () => {
     year: string
   }>()
 
-  const pathname = usePathname()
-  const router = useRouter()
   const currentTime = moment()
 
   const yearlySales = useQuery({
@@ -69,82 +45,10 @@ export const StoreSalesChart = () => {
       ),
   })
 
-  function onMonthSelect(month: string) {
-    const searchParams = new URLSearchParams(searchParamValues)
-    searchParams.set("month", month)
-    router.replace(`${pathname}?${searchParams.toString()}`)
-  }
-
-  function onMonthDeselect() {
-    const searchParams = new URLSearchParams(searchParamValues)
-    searchParams.delete("month")
-    router.replace(`${pathname}?${searchParams.toString()}`)
-  }
-
-  function onYearSelect(year: string) {
-    const searchParams = new URLSearchParams(searchParamValues)
-    searchParams.set("year", year)
-    router.replace(`${pathname}?${searchParams.toString()}`)
-  }
-
-  function onYearDeselect() {
-    const searchParams = new URLSearchParams(searchParamValues)
-    searchParams.delete("year")
-    router.replace(`${pathname}?${searchParams.toString()}`)
-  }
-
   return (
     <div className="w-full space-y-2 rounded-lg border p-4 shadow-sm lg:px-6 lg:py-4">
       <div className="flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-base font-medium lg:text-lg">Sales Report</h2>
-        <div className="flex items-center gap-2">
-          {chartMode === "year" && (
-            <Dropdown
-              placeholder="Select year"
-              options={getYearOptions()}
-              selectedOption={getYearOptions().find(
-                (option) => option.value.toString() === searchParamValues.year
-              )}
-              onOptionClick={(option) => onYearSelect(option.value as string)}
-              deselectOption={onYearDeselect}
-              className="w-32 lg:w-48"
-            />
-          )}
-          {chartMode === "month" && (
-            <Dropdown
-              placeholder="Select Month"
-              options={getMonthOptions()}
-              selectedOption={getMonthOptions().find(
-                (option) => option.value.toString() === searchParamValues.month
-              )}
-              onOptionClick={(option) => onMonthSelect(option.value as string)}
-              deselectOption={onMonthDeselect}
-              className="w-32 lg:w-48"
-            />
-          )}
-          <Button
-            size="sm"
-            onClick={() => setChartMode("year")}
-            className={clsx(
-              "lg:text-xs",
-              chartMode === "year" &&
-                "bg-gray-900 text-white  lg:hover:bg-gray-800"
-            )}
-          >
-            Year
-          </Button>
-          <Button
-            size="sm"
-            onClick={() => setChartMode("month")}
-            className={clsx(
-              "lg:text-xs",
-              chartMode === "month" &&
-                "bg-gray-900 text-white  lg:hover:bg-gray-800"
-            )}
-          >
-            Month
-          </Button>
-        </div>
+        <h2 className="text-base font-medium lg:text-lg">Total Sales</h2>
       </div>
       <ResponsiveContainer width="100%" height={200}>
         {chartMode === "year" ? (
