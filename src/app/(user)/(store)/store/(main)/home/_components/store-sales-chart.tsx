e7f31a86b626876b52edc6cd74fde319f35be4cd
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-06 16:15:11 +0700

    refactor(store/sales-chart): replace monthly and yearly sales with store sales

--- src/app/(user)/(store)/store/(main)/home/_components/store-sales-chart.tsx
+++ src/app/(user)/(store)/store/(main)/home/_components/store-sales-chart.tsx
@@ -2,8 +2,6 @@
 
 import { useSearchParamsValues } from "@/utils"
 import { useQuery } from "@tanstack/react-query"
-import moment from "moment"
-import { useState } from "react"
 import {
   Area,
   AreaChart,
@@ -11,38 +9,23 @@ import {
   ResponsiveContainer,
   Tooltip,
   XAxis,
+  YAxis,
 } from "recharts"
+import { TBaseDataFilterParams } from "../../../../../../../../types"
+import { getStoreSalesByTimeRange } from "../../../_actions"
 
-import { getStoreMonthlySales, getStoreYearlySales } from "../../../_actions"
-
-export const StoreSalesChart = () => {
-  const [chartMode, setChartMode] = useState<"month" | "year">("month")
-
-  const searchParamValues = useSearchParamsValues<{
-    month: string
-    year: string
-  }>()
-
-  const currentTime = moment()
-
-  const yearlySales = useQuery({
-    queryKey: ["yearlySales", searchParamValues],
-    queryFn: () =>
-      getStoreYearlySales(
-        searchParamValues.year
-          ? Number(searchParamValues.year)
-          : currentTime.get("year")
-      ),
-  })
-
-  const monthlySales = useQuery({
-    queryKey: ["montlySales", searchParamValues],
-    queryFn: () =>
-      getStoreMonthlySales(
-        searchParamValues.month
-          ? Number(searchParamValues.month)
-          : currentTime.get("month")
-      ),
+export const StoreSalesChart: React.FC<{ storeId: number }> = ({ storeId }) => {
+  const searchParamValues = useSearchParamsValues<TBaseDataFilterParams>()
+  const storeSales = useQuery({
+    queryKey: ["storeSales", searchParamValues],
+    queryFn: async () => {
+      const sales = await getStoreSalesByTimeRange(
+        storeId,
+        searchParamValues?.startDate,
+        searchParamValues?.endDate
+      )
+      return sales
+    },
   })
 
   return (
@@ -51,53 +34,24 @@ export const StoreSalesChart = () => {
         <h2 className="text-base font-medium lg:text-lg">Total Sales</h2>
       </div>
       <ResponsiveContainer width="100%" height={200}>
-        {chartMode === "year" ? (
-          <AreaChart
-            data={yearlySales.data?.map((sale) => ({
-              name: sale.month,
-              uv: sale.sales,
-            }))}
-            margin={{
-              top: 10,
-              right: 0,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </AreaChart>
-        ) : (
-          <AreaChart
-            data={monthlySales.data?.map((sale) => ({
-              name: sale.date,
-              uv: sale.sales,
-            }))}
-            margin={{
-              top: 10,
-              right: 0,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </AreaChart>
-        )}
+        <AreaChart
+          data={storeSales.data?.map((sale) => ({
+            name: sale.time,
+            uv: sale.sales,
+          }))}
+          margin={{
+            top: 10,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" fontSize={12} />
+          <YAxis fontSize={12} />
+          <Tooltip formatter={(value) => `Rp. ${value.toLocaleString("id")}`} />
+          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   )
