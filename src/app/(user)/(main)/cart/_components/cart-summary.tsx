410531612242f6b95cacf95930fc8965a16d27ac
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-11 15:18:05 +0700

    refactor(cart/summary): layout

--- src/app/(user)/(main)/cart/_components/cart-summary.tsx
+++ src/app/(user)/(main)/cart/_components/cart-summary.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { useRouter } from "next/navigation"
-
 import { Button } from "@/components/ui/button"
 
+import { ArrowRight } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { CartItem } from "../_types"
 
 interface CartCheckoutDialogProps {
@@ -12,34 +12,67 @@ interface CartCheckoutDialogProps {
 
 export const CartSummary = ({ selectedItems }: CartCheckoutDialogProps) => {
   const router = useRouter()
-  const total = selectedItems.reduce(
+  const subTotal = selectedItems.reduce(
     (firstValue, item) => item.amount * item.product.price + firstValue,
     0
   )
+  const taxFromSubTotal = subTotal * 0.11
+  const total = subTotal + taxFromSubTotal
+
+  function currencyFormat(value: number) {
+    return value.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    })
+  }
+
+  function onCheckout() {
+    localStorage.setItem("selectedCartItems", JSON.stringify(selectedItems))
+    router.push("/checkout")
+  }
 
   return (
-    <div className="sticky bottom-0 w-full bg-white dark:bg-neutral-950 lg:w-2/6 lg:px-8">
-      <div className="border-t border-gray-200 p-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.1)] dark:border-t-gray-800 lg:rounded-md lg:border lg:border-gray-200 lg:p-4 lg:shadow-sm lg:dark:border-gray-800">
-        <h5 className="hidden text-xs lg:inline lg:text-base">Cart Summary</h5>
-        <div className="mb-4 mt-2 flex justify-between">
-          <span className="text-xs text-slate-500 lg:text-sm">Subtotal</span>
-          <span className="text-sm lg:text-base">
-            Rp {total.toLocaleString("id")}
-          </span>
+    <div className="lg:sticky lg:top-28">
+      <div className="shadow-2xl shadow-gray-100/50 rounded-2xl border overflow-hidden">
+        <div className="border-b border-gray-100 bg-gray-50 p-4">
+          <div className="text-xl">Order Summary</div>
         </div>
-        <Button
-          disabled={!selectedItems.length}
-          onClick={() => {
-            localStorage.setItem(
-              "selectedCartItems",
-              JSON.stringify(selectedItems)
-            )
-            router.push("/cart/checkout")
-          }}
-          className="w-full disabled:hover:bg-gray-100 lg:text-xs"
-        >
-          Checkout
-        </Button>
+        <div className="space-y-6 p-6">
+          <div className="space-y-4">
+            <div className="flex justify-between text-gray-600">
+              <span>Subtotal</span>
+              <span>{currencyFormat(subTotal)}</span>
+            </div>
+            <div className="flex justify-between text-gray-600">
+              <span>Shipping</span>
+              <span>{currencyFormat(0)}</span>
+            </div>
+            <div className="flex justify-between text-gray-600">
+              <span>Tax (11%)</span>
+              <span>{currencyFormat(taxFromSubTotal)}</span>
+            </div>
+          </div>
+          <div className="border-t border-gray-100 pt-4">
+            <div className="flex justify-between text-lg font-semibold">
+              <span>Total</span>
+              <span>{currencyFormat(total)}</span>
+            </div>
+          </div>
+          <Button
+            variant="default"
+            size="lg"
+            className="w-full"
+            disabled={selectedItems.length === 0}
+            onClick={onCheckout}
+          >
+            Checkout
+            <ArrowRight className="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform" />
+          </Button>
+
+          <p className="text-center text-sm text-gray-500">
+            Free shipping on orders over Rp 1,000,000
+          </p>
+        </div>
       </div>
     </div>
   )
