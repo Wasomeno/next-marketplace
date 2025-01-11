a067edbc2a70584afbc56f4f2804de8b624a432c
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-11 15:18:51 +0700

    style(orders/card): layout

--- src/app/(user)/(main)/orders/_components/order-card.tsx
+++ src/app/(user)/(main)/orders/_components/order-card.tsx
@@ -30,16 +30,16 @@ export const OrderCard = ({ order, userEmail }: OrderCardProps) => {
   }
 
   return (
-    <div className="flex flex-col gap-2 rounded-md border bg-opacity-50 px-4 py-2.5 shadow-sm dark:border-gray-800 dark:bg-slate-950 dark:bg-opacity-50 dark:shadow-gray-800 ">
+    <div className="flex flex-col gap-2 rounded-md border bg-opacity-50 py-4 px-6 shadow-sm dark:border-gray-800 dark:bg-slate-950 dark:bg-opacity-50 dark:shadow-gray-800 ">
       <div className="flex items-center justify-between lg:h-10">
-        <div className="text-xs">{time}</div>
+        <div className="text-sm">{time}</div>
         <span className="rounded-md bg-blue-200 p-1.5 text-xs font-medium tracking-wide dark:bg-blue-900">
           {order.status.name}
         </span>
       </div>
       <div className="flex flex-wrap items-center gap-2 lg:gap-10">
-        <div className="flex w-full items-center gap-4 border-r-slate-200 dark:border-r-gray-800  lg:w-4/6 lg:border-r">
-          <div className="relative h-16 w-16 overflow-hidden rounded-md border dark:border-gray-800 lg:h-20 lg:w-20">
+        <div className="flex w-full items-center gap-6 border-r-slate-200 dark:border-r-gray-800  lg:w-4/6 lg:border-r">
+          <div className="relative h-16 w-16 overflow-hidden dark:border-gray-800 lg:h-48 lg:w-40">
             <Image
               src={order.products[0].product.featured_image_url}
               alt="product-image"
@@ -47,15 +47,17 @@ export const OrderCard = ({ order, userEmail }: OrderCardProps) => {
             />
           </div>
           <div className="space-y-2">
-            <div className="text-sm">{order.products[0].product.name}</div>
-            <div className="text-sm">
+            <div className="text-sm font-medium">
+              {order.products[0].product.name}
+            </div>
+            <div className="text-sm text-muted-foreground">
               {order.products[0].amount} x Rp{" "}
               {order.products[0].product.price.toLocaleString("id")}
             </div>
           </div>
         </div>
-        <div className="space-y-2">
-          <div className="text-xs font-medium lg:text-sm">Total</div>
+        <div className="space-y-1">
+          <div className="text-xs font-bold lg:text-sm">Total</div>
           <div className="text-xs lg:text-base">
             Rp.{" "}
             {order.products
