fd74fa699703e2ad773a5145702f5d6f90baa0a5
Arndev0x <60634068+Wasomeno@users.noreply.github.com>
2025-01-27 21:58:21 +0700

    refactor(order-status-dropdown): options

--- src/app/(user)/(main)/orders/_components/order-status-dropdown.tsx
+++ src/app/(user)/(main)/orders/_components/order-status-dropdown.tsx
@@ -1,25 +1,27 @@
 "use client"
 
-import React from "react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useSearchParamsValues } from "@/utils"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import React from "react"
 
+import { getOrderStatuses } from "@/actions/user/order"
 import { Dropdown, Option } from "@/components/dropdown"
-
-const statusOptions: Option[] = [
-  { label: "All", value: "" },
-  { label: "Awaiting Payment", value: "Awaiting Payment" },
-  { label: "Payment Confirmed", value: "Payment Confirmed" },
-  { label: "On Proccess", value: "On Proccess" },
-  { label: "On Shipping", value: "On Shipping" },
-  { label: "Arrived", value: "Arrived" },
-  { label: "Done", value: "Done" },
-]
+import { useQuery } from "@tanstack/react-query"
 
 export const OrderStatusDropdown = () => {
   const router = useRouter()
   const pathname = usePathname()
   const searchParamValues = useSearchParamsValues<{ status: string }>()
+
+  const orderStatuses = useQuery({
+    queryKey: ["order-statuses"],
+    queryFn: () => getOrderStatuses(),
+  })
+
+  const orderStatusOptions = orderStatuses.data?.map((status) => ({
+    label: status.name,
+    value: status.id,
+  }))
 
   function selectOption(option: Option) {
     const searchParams = new URLSearchParams(searchParamValues)
@@ -35,13 +37,14 @@ export const OrderStatusDropdown = () => {
 
   return (
     <Dropdown
-      options={statusOptions}
-      selectedOption={statusOptions.find(
-        (option) => option.value === searchParamValues.status
+      options={orderStatusOptions}
+      selectedOption={orderStatusOptions?.find(
+        (option) => option.value.toString() === searchParamValues.status
       )}
       placeholder="Select Status"
       onOptionClick={selectOption}
       deselectOption={deselectOption}
+      isLoading={orderStatuses.isLoading}
     />
   )
 }
